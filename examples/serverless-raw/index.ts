153d52863ac883a00a9a9f7b4edb245fc53908d5
Luke Hoban <luke@pulumi.com>
2017-07-18 20:15:15 -0700

    Get serverless-raw example running
    Some tweaks to API Gateway construction to enable the
    example to work correctly.

--- examples/serverless-raw/index.ts
+++ examples/serverless-raw/index.ts
@@ -2,24 +2,24 @@
 
 import * as aws from "@lumi/aws";
 import * as lumi from "@lumi/lumi";
-import {jsonStringify} from "@lumi/lumirt";
+import { jsonStringify } from "@lumi/lumirt";
 let region = aws.config.requireRegion();
 
 ///////////////////
 // Lambda Function
 ///////////////////
 let policy = {
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "lambda.amazonaws.com",
-      },
-      "Effect": "Allow",
-      "Sid": "",
-    },
-  ],
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Action": "sts:AssumeRole",
+            "Principal": {
+                "Service": "lambda.amazonaws.com",
+            },
+            "Effect": "Allow",
+            "Sid": "",
+        },
+    ],
 };
 
 let role = new aws.iam.Role("mylambda-role", {
@@ -30,59 +30,59 @@ let fullAccess = new aws.iam.RolePolicyAttachment("mylambda-access", {
     policyArn: aws.iam.AWSLambdaFullAccess,
 });
 let lambda = new aws.lambda.Function("mylambda", {
-  code: new lumi.asset.AssetArchive({
-    "index.js": new lumi.asset.String(
-        "exports.handler = (e, c, cb) => cb(null, {statusCode: 200, body: 'Hello, world!'});",
-    ),
-  }),
-  role: role.arn,
-  handler: "index.handler",
-  runtime: aws.lambda.NodeJS6d10Runtime,
+    code: new lumi.asset.AssetArchive({
+        "index.js": new lumi.asset.String(
+            "exports.handler = (e, c, cb) => cb(null, {statusCode: 200, body: 'Hello, world!'});",
+        ),
+    }),
+    role: role.arn,
+    handler: "index.handler",
+    runtime: aws.lambda.NodeJS6d10Runtime,
 });
 
 ///////////////////
 // Logging
 ///////////////////
 let logGroup = new aws.cloudwatch.LogGroup("/aws/lambda/mylambda", {
-  retentionInDays: 7,
+    retentionInDays: 7,
 });
 
 let logcollector = new aws.lambda.Function("mylambda-logcollector", {
-  code: new lumi.asset.AssetArchive({
-    "index.js": new lumi.asset.String(
-        "exports.handler = (e, c, cb) => console.log(e);",
-    ),
-  }),
-  role: role.arn,
-  handler: "index.handler",
-  runtime: aws.lambda.NodeJS6d10Runtime,
+    code: new lumi.asset.AssetArchive({
+        "index.js": new lumi.asset.String(
+            "exports.handler = (e, c, cb) => console.log(e);",
+        ),
+    }),
+    role: role.arn,
+    handler: "index.handler",
+    runtime: aws.lambda.NodeJS6d10Runtime,
 });
 
 let permission = new aws.lambda.Permission("logcollector-permission", {
-  action: "lambda:InvokeFunction",
-  principal: "logs." + region + ".amazonaws.com",
-  sourceArn: logGroup.arn + ":*",
-  function: logcollector,
+    action: "lambda:InvokeFunction",
+    principal: "logs." + region + ".amazonaws.com",
+    sourceArn: logGroup.arn,
+    function: logcollector,
 });
 
 let logSubscription = new aws.cloudwatch.LogSubscriptionFilter("logsubscription", {
-  destinationArn: logcollector.arn,
-  logGroup: logGroup,
-  filterPattern: "",
+    destinationArn: logcollector.arn,
+    logGroup: logGroup,
+    filterPattern: "",
 });
 
 ///////////////////
 // DynamoDB Table
 ///////////////////
 let music = new aws.dynamodb.Table("music", {
-  attribute: [
-    { name: "Album", type: "S" },
-    { name: "Artist", type: "S" },
-  ],
-  hashKey: "Album",
-  rangeKey: "Artist",
-  readCapacity: 1,
-  writeCapacity: 1,
+    attribute: [
+        { name: "Album", type: "S" },
+        { name: "Artist", type: "S" },
+    ],
+    hashKey: "Album",
+    rangeKey: "Artist",
+    readCapacity: 1,
+    writeCapacity: 1,
 });
 
 ///////////////////
@@ -112,23 +112,30 @@ let restApi = new aws.apigateway.RestApi("myrestapi", {});
 
 let resource = new aws.apigateway.Resource("myrestapi-resource", {
     restApi: restApi,
-    pathPart: "/bambam",
+    pathPart: "bambam",
     parentId: restApi.rootResourceId!,
+});
+
+let method = new aws.apigateway.Method("myrestapi-method", {
+    restApi: restApi,
+    resource: resource,
+    httpMethod: "ANY",
+    authorization: "NONE",
 });
 
 let integration = new aws.apigateway.Integration("myrestapi-integration", {
     restApi: restApi,
     resource: resource,
     httpMethod: "ANY",
-    type: "aws_proxy",
+    type: "AWS_PROXY",
     integrationHttpMethod: "POST",
     passthroughBehavior: "when_no_match",
     uri: "arn:aws:apigateway:" + region + ":lambda:path/2015-03-31/functions/" + lambda.arn + "/invocations",
 });
 
 let deployment = new aws.apigateway.Deployment("myrestapi-deployment-prod", {
-  restApi: restApi,
-  description: "my deployment",
-  stageName: "prod",
+    restApi: restApi,
+    description: "my deployment",
+    stageName: "prod",
 });
 
