01edec32dfe625fac48c6a9966d69fa25a15e6de
joeduffy <joe@pulumi.com>
2017-07-21 14:27:52 -0700

    Add a Go test for the examples directory

---
+++ examples/examples_test.go
@@ -0,0 +1,54 @@
+// Copyright 2016-2017, Pulumi Corporation.  All rights reserved.
+
+package examples
+
+import (
+	"os"
+	"path"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/pulumi/lumi/pkg/integrationtesting"
+)
+
+func TestExamples(t *testing.T) {
+	var examples []string
+	region := os.Getenv("AWS_REGION")
+	if region == "" {
+		t.Skipf("Skipping test due to missing AWS_REGION environment variable")
+	}
+	cwd, err := os.Getwd()
+	if !assert.NoError(t, err, "expected a valid working directory: %v", err) {
+		return
+	}
+	if testing.Short() {
+		examples = []string{
+			path.Join(cwd, "minimal"),
+		}
+	} else {
+		examples = []string{
+			path.Join(cwd, "minimal"),
+			path.Join(cwd, "webserver"),
+			path.Join(cwd, "webserver-comp"),
+			path.Join(cwd, "beanstalk"),
+			path.Join(cwd, "cpuwatch"),
+		}
+	}
+	options := integrationtesting.LumiProgramTestOptions{
+		Config: map[string]string{
+			"aws:config:region": region,
+		},
+		Dependencies: []string{
+			"@lumi/lumirt",
+			"@lumi/lumi",
+			"@lumi/aws",
+		},
+	}
+	for _, ex := range examples {
+		example := ex
+		t.Run(example, func(t *testing.T) {
+			integrationtesting.LumiProgramTest(t, example, options)
+		})
+	}
+}
