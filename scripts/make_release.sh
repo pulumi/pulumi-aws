9c8442c93f607d81686bfc4af3df6466101c3f57
Joe Duffy <joe@pulumi.com>
2017-09-07 11:14:49 -0700

    Adopt the new publish/install scripts (#17)

---
+++ scripts/make_release.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# make_release.sh will create a build package ready for publishing.
+set -e
+
+ROOT=$(dirname $0)/..
+PUBDIR=$(mktemp -du)
+GITVER=$(git rev-parse HEAD)
+PUBFILE=$(dirname ${PUBDIR})/${GITVER}.tgz
+declare -a PUBTARGETS=(${GITVER} $(git describe --tags 2>/dev/null) $(git rev-parse --abbrev-ref HEAD))
+
+# Copy the pack.
+cp -R ${ROOT}/pack/. ${PUBDIR}/
+echo . >> ${PUBDIR}/packs.txt
+
+# Tar up the file and then print it out for use by the caller or script.
+tar -czf ${PUBFILE} -C ${PUBDIR} .
+echo ${PUBFILE} ${PUBTARGETS[@]}
+
--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,43 +1,13 @@
 #!/bin/bash
-# publish.sh will publish the current bits from the usual build location to an S3 build share.
-
+# publish.sh builds and publishes a release.
 set -e
 
-# Default the Lumi root to the right place, but let it be overridden.
-if [ -z "${LUMIROOT}" ]; then
-    LUMIROOT=/usr/local/lumi
+PUBLISH=$GOPATH/src/github.com/pulumi/pulumi/scripts/publish.sh
+if [ ! -f $PUBLISH ]; then
+    >&2 echo "error: Missing publish script at $PUBLISH"
+    exit 1
 fi
-LUMILIB=${LUMIROOT}/packs
 
-PUBDIR=$(mktemp -du)
-GITVER=$(git rev-parse HEAD)
-PUBFILE=$(dirname ${PUBDIR})/${GITVER}.tgz
-PUBPREFIX=s3://eng.pulumi.com/releases/aws
-declare -a PUBTARGETS=(${GITVER} $(git describe --tags 2>/dev/null) $(git rev-parse --abbrev-ref HEAD))
-
-ROOT=$(dirname $0)/..
-
-# Make sure the repo isn't dirty.
-git diff-index --quiet HEAD -- || \
-    test -n "${PUBFORCE}" || \
-    (echo "error: Cannot publish a dirty repo; set PUBFORCE=true to override" && exit 99)
-
-# If it isn't, or publication was forced, do it.
-mkdir -p ${PUBDIR}/cmd ${PUBDIR}/packs
-cp -R ${LUMILIB}/aws ${PUBDIR}/packs/aws
-
-# Tar up the release and upload it to our S3 bucket.
-tar -chzf ${PUBFILE} -C ${PUBDIR} .
-for target in ${PUBTARGETS[@]}; do
-    PUBTARGET=${PUBPREFIX}/${target}.tgz
-    echo Publishing ${GITVER} to: ${PUBTARGET}
-    if [ -z "${FIRSTTARGET}" ]; then
-        # Upload the first one for real.
-        aws s3 cp ${PUBFILE} ${PUBTARGET}
-        FIRSTTARGET=${PUBTARGET}
-    else
-        # For all others, reuse the first target to avoid re-uploading.
-        aws s3 cp ${FIRSTTARGET} ${PUBTARGET}
-    fi
-done
+RELEASE_INFO=($(./make_release.sh))
+${PUBLISH} ${RELEASE_INFO[0]} aws ${RELEASE_INFO[@]:1}
 
