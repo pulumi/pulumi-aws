30afd16d51e642dda2067fa848af7ec1a7443659
joeduffy <joe@pulumi.com>
2017-09-07 14:25:12 -0700

    Publish releases from bin/

--- scripts/make_release.sh
+++ scripts/make_release.sh
@@ -9,7 +9,7 @@ PUBFILE=$(dirname ${PUBDIR})/${GITVER}.tgz
 declare -a PUBTARGETS=(${GITVER} $(git describe --tags 2>/dev/null) $(git rev-parse --abbrev-ref HEAD))
 
 # Copy the pack.
-cp -R ${ROOT}/pack/. ${PUBDIR}/
+cp -R ${ROOT}/pack/bin/. ${PUBDIR}/
 echo . >> ${PUBDIR}/packs.txt
 
 # Tar up the file and then print it out for use by the caller or script.
