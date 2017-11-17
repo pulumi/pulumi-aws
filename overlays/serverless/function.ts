200061fe925ce62939831aad6320ae61689ed6b2
joeduffy <joe@pulumi.com>
2017-11-17 14:36:06 -0800

    Adopt new resource parenting approach

--- overlays/serverless/function.ts
+++ overlays/serverless/function.ts
@@ -27,40 +27,6 @@ export interface Context {
  */
 export type Handler = (event: any, context: Context, callback: (error: any, result: any) => void) => any;
 
-let policy = {
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Action": "sts:AssumeRole",
-            "Principal": {
-                "Service": "lambda.amazonaws.com",
-            },
-            "Effect": "Allow",
-            "Sid": "",
-        },
-    ],
-};
-
-function createJavaScriptLambda(
-    functionName: string, role: Role,
-    closure: Promise<pulumi.runtime.Closure>, opts: FunctionOptions): lambda.Function {
-    return new lambda.Function(functionName, {
-        code: new pulumi.asset.AssetArchive({
-            // TODO[pulumi/pulumi-aws#35] We may want to allow users to control what gets uploaded. Currently, we
-            // upload the entire folder as there may be dependencies on any files here.
-            ".": new pulumi.asset.FileArchive("."),
-            "__index.js": new pulumi.asset.StringAsset(closure.then(pulumi.runtime.serializeJavaScriptText)),
-        }),
-        handler: "__index.handler",
-        runtime: lambda.NodeJS6d10Runtime,
-        role: role.arn,
-        timeout: opts.timeout === undefined ? 180 : opts.timeout,
-        memorySize: opts.memorySize,
-        deadLetterConfig: opts.deadLetterConfig === undefined ? undefined : [ opts.deadLetterConfig ],
-        vpcConfig: opts.vpcConfig === undefined ? undefined : [ opts.vpcConfig ],
-    });
-}
-
 export interface FunctionOptions {
     policies: ARN[];
     timeout?: number;
@@ -82,7 +48,8 @@ export class Function extends pulumi.ComponentResource {
     public readonly role: Role;
     public readonly policies: RolePolicyAttachment[];
 
-    constructor(name: string, options: FunctionOptions, func: Handler) {
+    constructor(name: string, options: FunctionOptions, func: Handler,
+                parent?: pulumi.Resource, dependsOn?: pulumi.Resource[]) {
         if (!name) {
             throw new Error("Missing required resource name");
         }
@@ -90,41 +57,55 @@ export class Function extends pulumi.ComponentResource {
             throw new Error("Missing required function callback");
         }
 
-        let role: Role;
-        let policies: RolePolicyAttachment[];
-        let lambda: lambda.Function;
-        super(
-            "aws:serverless:Function",
-            name,
-            {
-                options: options,
-            },
-            () => {
-                // Attach a role and then, if there are policies, attach those too.
-                role = new Role(name + "-iamrole", {
-                    assumeRolePolicy: JSON.stringify(policy),
-                });
+        super("aws:serverless:Function", name, { options: options }, parent, dependsOn);
 
-                policies = [];
-                for (let i = 0; i < options.policies.length; i++) {
-                    let attachment = new RolePolicyAttachment(name + "-iampolicy-" + i, {
-                        role: role,
-                        policyArn: options.policies[i],
-                    });
-                    policies.push(attachment);
-                }
+        // Attach a role and then, if there are policies, attach those too.
+        this.role = new Role(name + "-iamrole", {
+            assumeRolePolicy: JSON.stringify(lambdaRolePolicy),
+        }, this);
 
-                // Now compile the function text into an asset we can use to create the lambda.
-                let closure: Promise<pulumi.runtime.Closure> = pulumi.runtime.serializeClosure(func);
-                if (!closure) {
-                    throw new Error("Failed to serialize function closure");
-                }
-                lambda = createJavaScriptLambda(name, role, closure, options);
-            },
-        );
-        this.role = role;
-        this.policies = policies;
-        this.lambda = lambda;
+        this.policies = [];
+        for (let i = 0; i < options.policies.length; i++) {
+            let attachment = new RolePolicyAttachment(name + "-iampolicy-" + i, {
+                role: this.role,
+                policyArn: options.policies[i],
+            }, this);
+            this.policies.push(attachment);
+        }
+
+        // Now compile the function text into an asset we can use to create the lambda.
+        let closure: Promise<pulumi.runtime.Closure> = pulumi.runtime.serializeClosure(func);
+        if (!closure) {
+            throw new Error("Failed to serialize function closure");
+        }
+        this.lambda = new lambda.Function(name, {
+            code: new pulumi.asset.AssetArchive({
+                // TODO[pulumi/pulumi-aws#35] We may want to allow users to control what gets uploaded. Currently, we
+                //     upload the entire folder as there may be dependencies on any files here.
+                ".": new pulumi.asset.FileArchive("."),
+                "__index.js": new pulumi.asset.StringAsset(closure.then(pulumi.runtime.serializeJavaScriptText)),
+            }),
+            handler: "__index.handler",
+            runtime: lambda.NodeJS6d10Runtime,
+            role: this.role.arn,
+            timeout: options.timeout === undefined ? 180 : options.timeout,
+            memorySize: options.memorySize,
+            deadLetterConfig: options.deadLetterConfig === undefined ? undefined : [ options.deadLetterConfig ],
+            vpcConfig: options.vpcConfig === undefined ? undefined : [ options.vpcConfig ],
+        }, this);
     }
 }
 
+const lambdaRolePolicy = {
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Action": "sts:AssumeRole",
+            "Principal": {
+                "Service": "lambda.amazonaws.com",
+            },
+            "Effect": "Allow",
+            "Sid": "",
+        },
+    ],
+};
