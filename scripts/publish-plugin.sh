a9b13ab8c7c6d99ae4513313544c0dcd4533e76f
joeduffy <joe@pulumi.com>
2018-02-20 16:43:36 -0800

    Move @pulumi/aws onto a plugins plan

--- scripts/publish-plugin.sh
+++ scripts/publish-plugin.sh
@@ -6,7 +6,7 @@ set -o nounset -o errexit -o pipefail
 ROOT=$(dirname $0)/..
 WORK_PATH=$(mktemp -d)
 VERSION=$(git describe --tags --dirty 2>/dev/null)
-PLUGIN_PACKAGE_NAME="pulumi-provider-aws-${VERSION}-$(go env GOOS)-$(go env GOARCH).tar.gz"
+PLUGIN_PACKAGE_NAME="pulumi-resource-aws-${VERSION}-$(go env GOOS)-$(go env GOARCH).tar.gz"
 PLUGIN_PACKAGE_DIR="$(mktemp -d)"
 PLUGIN_PACKAGE_PATH="${PLUGIN_PACKAGE_DIR}/${PLUGIN_PACKAGE_NAME}"
 
@@ -18,8 +18,8 @@ fi
 
 go build \
    -ldflags "-X github.com/pulumi/pulumi-aws/pkg/version.Version=${VERSION}" \
-   -o "${WORK_PATH}/pulumi-provider-aws${BIN_SUFFIX}" \
-   "${ROOT}/cmd/pulumi-provider-aws"
+   -o "${WORK_PATH}/pulumi-resource-aws${BIN_SUFFIX}" \
+   "${ROOT}/cmd/pulumi-resource-aws"
 
 # Tar up the plugin
 tar -czf ${PLUGIN_PACKAGE_PATH} -C ${WORK_PATH} .
@@ -27,7 +27,7 @@ tar -czf ${PLUGIN_PACKAGE_PATH} -C ${WORK_PATH} .
 # rel.pulumi.com is in our production account, so assume that role first
 CREDS_JSON=$(aws sts assume-role \
                  --role-arn "arn:aws:iam::058607598222:role/UploadPulumiReleases" \
-                 --role-session-name "upload-plugin-pulumi-provider-aws" \
+                 --role-session-name "upload-plugin-pulumi-resource-aws" \
                  --external-id "upload-pulumi-release")
 
 # Use the credentials we just assumed
