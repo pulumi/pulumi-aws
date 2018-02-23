838114b09678dded8f44794387b6e257ae9cf148
joeduffy <joe@pulumi.com>
2018-02-23 07:21:41 -0800

    Simplify Python example and use exports

--- examples/webserver-py/__main__.py
+++ examples/webserver-py/__main__.py
@@ -1,20 +1,20 @@
 # Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
+import pulumi
 from pulumi_aws import ec2
 from ami import get_linux_ami
 
 size = 't2.micro'
 
-def main():
-    group = ec2.SecurityGroup('web-secgrp',
-        description='Enable HTTP access',
-        ingress=[
-            { 'protocol': 'tcp', 'from_port': 80, 'to_port': 80, 'cidr_blocks': ['0.0.0.0/0'] }
-        ])
-    server = ec2.Instance('web-server-www',
-        instance_type=size,
-        security_groups=[group],
-        ami=get_linux_ami(size))
+group = ec2.SecurityGroup('web-secgrp',
+    description='Enable HTTP access',
+    ingress=[
+        { 'protocol': 'tcp', 'from_port': 80, 'to_port': 80, 'cidr_blocks': ['0.0.0.0/0'] }
+    ])
+server = ec2.Instance('web-server-www',
+    instance_type=size,
+    security_groups=[group],
+    ami=get_linux_ami(size))
 
-if __name__ == '__main__':
-    main()
+pulumi.export('public_ip', server.public_ip)
+pulumi.export('public_dns', server.public_dns)
