064bc3d04ab5d81f32d25adcd313dda480c3e704
Matt Ellis <matt@pulumi.com>
2018-03-11 20:08:22 -0700

    Update to v0.11.0-rc1

--- resources.go
+++ resources.go
@@ -1287,7 +1287,7 @@ func Provider() tfbridge.ProviderInfo {
 				"@types/node": "^8.0.25", // so we can access strongly typed node definitions.
 			},
 			PeerDependencies: map[string]string{
-				"@pulumi/pulumi": "^0.11.0-dev-163-g89067313",
+				"@pulumi/pulumi": "^0.11.0-rc1",
 			},
 		},
 	}
