75cc3dc83c82299139e6e9fe689844e6c4686e82
Matt Ellis <matt@pulumi.com>
2018-04-05 15:03:45 -0700

    Publishing Improvements
    1. Don't publish NPM and PyPI packages until after the tests have
    passed. This means we won't publish untested packages and when tests
    fail we can re-try the Travis job if we'd like, without NPM erroring
    out because a package has already been published.
    
    2. Adopt the changes in pulumi/pulumi to not tag pre-release builds
    with the "latest" tag on NPM.

--- Makefile
+++ Makefile
@@ -50,14 +50,19 @@ install::
 test_all::
 	PATH=$(PULUMI_BIN):$(PATH) go test -v -cover -timeout 1h -parallel ${TESTPARALLELISM} ./examples
 
-.PHONY: publish
-publish:
+.PHONY: publish_tgz
+publish_tgz:
 	$(call STEP_MESSAGE)
-	./scripts/publish.sh
+	./scripts/publish_tgz.sh
+
+.PHONY: publish_packages
+publish_packages:
+	$(call STEP_MESSAGE)
+	./scripts/publish_packages.sh
 
 # The travis_* targets are entrypoints for CI.
 .PHONY: travis_cron travis_push travis_pull_request travis_api
 travis_cron: all
-travis_push: only_build publish only_test
+travis_push: only_build publish_tgz only_test publish_packages
 travis_pull_request: all
 travis_api: all
---
+++ scripts/publish_tgz.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+# publish_tgz.sh builds and publishes the tarballs that our other repositories consume.
+set -o nounset -o errexit -o pipefail
+
+ROOT=$(dirname $0)/..
+PUBLISH=$GOPATH/src/github.com/pulumi/home/scripts/publish.sh
+PUBLISH_GOOS=("linux" "windows" "darwin")
+PUBLISH_GOARCH=("amd64")
+PUBLISH_PROJECT="pulumi-aws"
+
+if [ ! -f $PUBLISH ]; then
+    >&2 echo "error: Missing publish script at $PUBLISH"
+    exit 1
+fi
+
+echo "Publishing SDK build to s3://eng.pulumi.com/:"
+for OS in "${PUBLISH_GOOS[@]}"
+do
+    for ARCH in "${PUBLISH_GOARCH[@]}"
+    do
+        export GOOS=${OS}
+        export GOARCH=${ARCH}
+
+        RELEASE_INFO=($($(dirname $0)/make_release.sh))
+        ${PUBLISH} ${RELEASE_INFO[0]} "${PUBLISH_PROJECT}/${OS}/${ARCH}" ${RELEASE_INFO[@]:1}
+    done
+done
+
+echo "Publishing Plugin archive to s3://rel.pulumi.com/:"
+for OS in "${PUBLISH_GOOS[@]}"
+do
+    for ARCH in "${PUBLISH_GOARCH[@]}"
+    do
+        export GOOS=${OS}
+        export GOARCH=${ARCH}
+
+        ${ROOT}/scripts/publish-plugin.sh
+    done
+done
