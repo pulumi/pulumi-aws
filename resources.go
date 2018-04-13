a59bb8f537998abc7b15cb751c3387a3f870305f
Matt Ellis <matt@pulumi.com>
2018-04-13 15:13:20 -0700

    Pick up v0.11.3-rc4 of @pulumi/pulumi

--- resources.go
+++ resources.go
@@ -1289,12 +1289,12 @@ func Provider() tfbridge.ProviderInfo {
 				"@types/node": "^8.0.25", // so we can access strongly typed node definitions.
 			},
 			PeerDependencies: map[string]string{
-				"@pulumi/pulumi": "^0.11.3-rc3",
+				"@pulumi/pulumi": "^0.11.3-rc4",
 			},
 		},
 		Python: &tfbridge.PythonInfo{
 			Requires: map[string]string{
-				"pulumi": ">=0.11.3-rc3",
+				"pulumi": ">=0.11.3-rc4",
 			},
 		},
 	}
