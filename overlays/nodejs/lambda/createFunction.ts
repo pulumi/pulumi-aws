06c40bb7b07cd5d9bb394234806cbc6f92acaa8c
Cyrus Najmabadi <cyrus@pulumi.com>
2018-05-01 16:40:13 -0700

    IMprove overlay.

--- overlays/nodejs/lambda/createFunction.ts
+++ overlays/nodejs/lambda/createFunction.ts
@@ -3,6 +3,7 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as lambda from "./function";
 import * as runtimes from "./runtimes";
+import * as utils from "../utils";
 
 /**
  * Context is the shape of the context object passed to a Function callback.
@@ -26,11 +27,15 @@ export interface Context {
  */
 export type Handler<TArg, TResult> = (event: TArg, context: Context, callback: (error: any, result: TResult) => void) => any;
 
+// User sholud not supply 'code' or 'handler' to createFunction.  We will create those
+// out of the handler they pass in.
+export type CallbackFunctionArgs = utils.Omit<lambda.FunctionArgs, "code" | "handler">
+
 /**
  * Helper to create an aws lambda function out of an actual javascript callback.
  */
 export function createFunction<TArg, TResult>(
-    name: string, handler: Handler<TArg, TResult>, args: lambda.FunctionArgs,
+    name: string, handler: Handler<TArg, TResult>, args: CallbackFunctionArgs,
     serialize?: (obj: any) => boolean, opts?: pulumi.ResourceOptions): lambda.Function<TArg, TResult> {
 
     if (!name) {
@@ -41,6 +46,12 @@ export function createFunction<TArg, TResult>(
     }
     if (!args.role) {
         throw new Error("Missing 'role' in 'args'");
+    }
+    if ((<any>args).code) {
+        throw new Error("'code' property should not be provided in 'args'");
+    }
+    if ((<any>args).handler) {
+        throw new Error("'handler' property should not be provided in 'args'");
     }
 
     // Now compile the function text into an asset we can use to create the lambda. Note: to
@@ -56,10 +67,8 @@ export function createFunction<TArg, TResult>(
         throw new Error("Failed to serialize function closure");
     }
 
-    const argsCopy = {
+    const argsCopy: lambda.FunctionArgs = {
         ...args,
-        runtime: args.runtime || runtimes.NodeJS8d10Runtime,
-        timeout: args.timeout === undefined ? 180 : args.timeout,
         code: new pulumi.asset.AssetArchive({
             // TODO[pulumi/pulumi-aws#35] We may want to allow users to control what gets uploaded. Currently, we
             //     upload the entire folder as there may be dependencies on any files here.
