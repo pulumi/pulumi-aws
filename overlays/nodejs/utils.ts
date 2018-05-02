938abf925d26b7954aaa3d9821347faa80abb547
Cyrus Najmabadi <cyrus@pulumi.com>
2018-05-02 14:07:40 -0700

    Don't require so high a TS version.

--- overlays/nodejs/utils.ts
+++ overlays/nodejs/utils.ts
@@ -4,4 +4,5 @@
 // Helpers to create 'subtraction' types.  i.e. a type equivalent to some other object type,
 // just with some members removed.
 
-export type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>
+export type Diff<T extends string, U extends string> = ({[P in T]: P } & {[P in U]: never } & { [x: string]: never })[T]
+export type Omit<T, K extends keyof T> = Pick<T, Diff<keyof T, K>>
