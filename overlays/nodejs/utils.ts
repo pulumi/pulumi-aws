8b77e5ca6973e8ec796203216c7709729dff44cf
Cyrus Najmabadi <cyrus@pulumi.com>
2018-05-07 12:10:54 -0700

    Revert "Extract code for converting a JS callback into an AWS function into its own location."
    This reverts commit 85ae2b74d8a88a5174d431a70cf1af98a3831762.

--- overlays/nodejs/utils.ts
+++
@@ -1,8 +0,0 @@
-// Copyright 2016-2017, Pulumi Corporation.  All rights reserved.
-
-// From: https://github.com/Microsoft/TypeScript/issues/12215
-// Helpers to create 'subtraction' types.  i.e. a type equivalent to some other object type,
-// just with some members removed.
-
-export type Diff<T extends string, U extends string> = ({[P in T]: P } & {[P in U]: never } & { [x: string]: never })[T]
-export type Omit<T, K extends keyof T> = Pick<T, Diff<keyof T, K>>
