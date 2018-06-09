bb640fa29afdad04d26ed90f132731999c280d37
joeduffy <joe@pulumi.com>
2018-06-08 17:32:05 -0700

    Fix up some missed /pack/ references

--- scripts/make_release.sh
+++ scripts/make_release.sh
@@ -44,7 +44,7 @@ copy_package() {
 run_go_build "${ROOT}/cmd/pulumi-resource-aws"
 
 # Copy Packages
-copy_package "${ROOT}/pack/nodejs/bin/." "@pulumi/aws"
+copy_package "${ROOT}/sdk/nodejs/bin/." "@pulumi/aws"
 
 # Tar up the file and then print it out for use by the caller or script.
 tar -czf ${PUBFILE} -C ${PUBDIR} .
