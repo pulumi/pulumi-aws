761359e1dadc596d942e153c9a6b67d7c93b1ced
Matt Ellis <matt@pulumi.com>
2018-06-11 14:48:11 -0700

    Publish package to test.pypi.com

--- scripts/publish_packages.sh
+++ scripts/publish_packages.sh
@@ -35,7 +35,7 @@ if [[ "${TRAVIS_OS_NAME:-}" == "linux" ]]; then
     # Next, publish the PyPI package.
     echo "Publishing Pip package to pulumi.com:"
     twine upload \
-        --repository-url https://pypi.pulumi.com?token=${PULUMI_API_TOKEN} \
-        -u pulumi -p pulumi \
+        --repository-url https://test.pypi.org/legacy/ \
+        -u pulumi -p ${PYPI_TEST_PASSWORD} \
         ${ROOT}/sdk/python/bin/dist/*.tar.gz
 fi
