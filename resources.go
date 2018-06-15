82334c280770e23c986945078d964780e51c209f
Matt Ellis <matt@pulumi.com>
2018-06-15 11:34:06 -0700

    Update package metadata
    Include a license property and update the homepage to the root of
    http://pulumi.io (the existing link 404'd).

--- resources.go
+++ resources.go
@@ -189,7 +189,8 @@ func Provider() tfbridge.ProviderInfo {
 		Name:        "aws",
 		Description: "A Pulumi package for creating and managing Amazon Web Services (AWS) cloud resources.",
 		Keywords:    []string{"pulumi", "aws"},
-		Homepage:    "https://pulumi.io/aws",
+		License:     "Apache 2.0",
+		Homepage:    "https://pulumi.io",
 		Repository:  "https://github.com/pulumi/pulumi-aws",
 		Config: map[string]*tfbridge.SchemaInfo{
 			"region": {
