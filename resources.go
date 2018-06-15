d51dd90f1544bc1de64dd61fb9c6aea4fd110274
Matt Ellis <matt@pulumi.com>
2018-06-15 14:58:28 -0700

    Update to 0.14.0

--- resources.go
+++ resources.go
@@ -1401,7 +1401,7 @@ func Provider() tfbridge.ProviderInfo {
 		},
 		JavaScript: &tfbridge.JavaScriptInfo{
 			Dependencies: map[string]string{
-				"@pulumi/pulumi":    "^0.14.0-rc1",
+				"@pulumi/pulumi":    "^0.14.0",
 				"builtin-modules":   "3.0.0",
 				"read-package-tree": "^5.2.1",
 				"resolve":           "^1.7.1",
@@ -1456,7 +1456,7 @@ func Provider() tfbridge.ProviderInfo {
 		},
 		Python: &tfbridge.PythonInfo{
 			Requires: map[string]string{
-				"pulumi": ">=0.14.0rc1,<0.15.0",
+				"pulumi": ">=0.14.0,<0.15.0",
 			},
 		},
 	}
