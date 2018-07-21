c3d5d3f284de127ee783b2d0959c9bf8cb549ff1
James Nugent <james@jen20.com>
2018-07-20 23:36:01 -0500

    Use aws.Tags type for aws.rds.ParameterGroup

--- resources.go
+++ resources.go
@@ -1138,6 +1138,7 @@ func Provider() tfbridge.ProviderInfo {
 					"description": {
 						Default: managedByPulumi,
 					},
+					"tags": {Type: awsType(awsMod, "Tags")},
 				},
 			},
 			"aws_db_security_group": {
