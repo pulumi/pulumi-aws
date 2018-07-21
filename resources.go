82244269866d21b794434447ea7000910580093d
James Nugent <james@jen20.com>
2018-07-21 00:11:31 -0500

    Use aws.Tags type for aws.ec2.DefaultSecurityGroup

--- resources.go
+++ resources.go
@@ -733,6 +733,7 @@ func Provider() tfbridge.ProviderInfo {
 					// Use "ingress" instead of "ingresses" to match AWS APIs
 					"ingress": {Name: "ingress"},
 					"egress":  {Name: "egress"},
+					"tags":    {Type: awsType(awsMod, "Tags")},
 				},
 			},
 			"aws_security_group_rule":               {Tok: awsResource(ec2Mod, "SecurityGroupRule")},
