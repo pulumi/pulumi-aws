ab05b7feed8686e692fa101af0e1889f091eb5a5
Cyrus Najmabadi <cyrus@pulumi.com>
2018-08-01 14:43:46 -0700

    Update serverless example to use new module capturing semantics.

--- tests/serverless_functions/index.ts
+++ tests/serverless_functions/index.ts
@@ -2,20 +2,25 @@
 
 import * as aws from "@pulumi/aws";
 import * as other from "./other/index"
+import * as awsSdk from "aws-sdk";
+import * as express from "express";
+import * as os from "os";
+import * as slack from "@slack/client";
+import * as mime from "mime-types";
 
 // Validate that 'require'd packages are captured correctly.
 function getContentType() {
-  let mime = require('mime-types');
   return mime.contentType(".js");
 }
+
 const testFunc = new aws.serverless.Function("f", {
   policies: [aws.iam.AWSLambdaFullAccess],
   includePaths: ['./Pulumi.yaml'],
 }, async (ev, ctx, cb) => {
-  var aws = await import('aws-sdk');
-  var express = await import('express');
-  var os = require('os');
-  var slack = require('@slack/client');
+  const client = new slack.WebClient("token");
+  const cpus = os.cpus();
+  const expr = express();
+  const s3 = new awsSdk.S3();
 
   var answer = other.answer;
   console.log(answer);
