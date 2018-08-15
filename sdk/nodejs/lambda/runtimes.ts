e2bfdb06b20f4d1fd27852c2d466396ce275762c
JT <justin@justintaylor.net>
2018-08-15 22:42:41 +1000

    Fixed typo that missed last commit.

--- sdk/nodejs/lambda/runtimes.ts
+++ sdk/nodejs/lambda/runtimes.ts
@@ -31,7 +31,7 @@ export type Runtime =
 
 export let DotnetCore1d0Runtime: Runtime = "dotnetcore1.0";
 export let DotnetCore2d0Runtime: Runtime = "dotnetcore2.0";
-export let DotnetCore2d01untime: Runtime = "dotnetcore2.1";
+export let DotnetCore2d1Runtime: Runtime = "dotnetcore2.1";
 export let Go1dxRuntime: Runtime = "go1.x";
 export let Java8Runtime: Runtime = "java8";
 export let NodeJS4d3EdgeRuntime: Runtime = "nodejs4.3-edge";
