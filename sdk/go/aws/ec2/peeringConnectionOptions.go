00ff7b1cf2a5fec85b6e826401a709ae35a90e12
James Nugent <james@jen20.com>
2018-11-29 16:54:02 -0600

    Run tfgen with latest pulumi-terraform

--- sdk/go/aws/ec2/peeringConnectionOptions.go
+++ sdk/go/aws/ec2/peeringConnectionOptions.go
@@ -85,6 +85,55 @@ import (
 // data "aws_caller_identity" "peer" {
 //   provider = "aws.accepter"
 // }
+// 
+// # Requester's side of the connection.
+// resource "aws_vpc_peering_connection" "peer" {
+//   provider = "aws.requester"
+// 
+//   vpc_id        = "${aws_vpc.main.id}"
+//   peer_vpc_id   = "${aws_vpc.peer.id}"
+//   peer_owner_id = "${data.aws_caller_identity.peer.account_id}"
+//   auto_accept   = false
+// 
+//   tags {
+//     Side = "Requester"
+//   }
+// }
+// 
+// # Accepter's side of the connection.
+// resource "aws_vpc_peering_connection_accepter" "peer" {
+//   provider = "aws.accepter"
+// 
+//   vpc_peering_connection_id = "${aws_vpc_peering_connection.peer.id}"
+//   auto_accept               = true
+// 
+//   tags {
+//     Side = "Accepter"
+//   }
+// }
+// 
+// resource "aws_vpc_peering_connection_options" "requester" {
+//   provider = "aws.requester"
+// 
+//   # As options can't be set until the connection has been accepted
+//   # create an explicit dependency on the accepter.
+//   vpc_peering_connection_id = "${aws_vpc_peering_connection_accepter.peer.id}"
+// 
+//   requester {
+//     allow_remote_vpc_dns_resolution = true
+//   }
+// }
+// 
+// resource "aws_vpc_peering_connection_options" "accepter" {
+//   provider = "aws.accepter"
+// 
+//   vpc_peering_connection_id = "${aws_vpc_peering_connection_accepter.peer.id}"
+// 
+//   accepter {
+//     allow_remote_vpc_dns_resolution = true
+//   }
+// }
+// ```
 type PeeringConnectionOptions struct {
 	s *pulumi.ResourceState
 }
