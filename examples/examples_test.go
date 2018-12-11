e777d1c8d4ba2f09b57d81ca93f21b8df9ff553b
Cyrus Najmabadi <cyrusn@pulumi.com>
2018-12-11 15:21:01 -0800

    Disable jaeger tests.

--- examples/examples_test.go
+++ examples/examples_test.go
@@ -31,7 +31,6 @@ func TestExamples(t *testing.T) {
 		Config: map[string]string{
 			"aws:region": region,
 		},
-		Tracing: "https://tracing.pulumi-engineering.com/collector/api/v1/spans",
 	}
 	baseJS := base.With(integration.ProgramTestOptions{
 		Dependencies: []string{
