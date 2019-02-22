e4f0881bebece26e25f4ce321b0de2f41f3ee1cd
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2019-02-22 11:19:03 -0800

    Flow downstream

--- resources.go
+++ resources.go
@@ -1849,7 +1849,7 @@ func Provider() tfbridge.ProviderInfo {
 		},
 		JavaScript: &tfbridge.JavaScriptInfo{
 			Dependencies: map[string]string{
-				"@pulumi/pulumi":    "features/all",
+				"@pulumi/pulumi":    "feature-all",
 				"aws-sdk":           "^2.0.0",
 				"mime":              "^2.0.0",
 				"builtin-modules":   "3.0.0",
