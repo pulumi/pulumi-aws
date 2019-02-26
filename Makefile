aad10c600bc4c4d0b7e96b871eb3ca676d716b66
CyrusNajmabadi <cyrus.najmabadi@gmail.com>
2019-02-25 20:29:31 -0800

    Make makefile consistent (#484)

--- Makefile
+++ Makefile
@@ -83,5 +83,5 @@ check_clean_worktree:
 .PHONY: travis_cron travis_push travis_pull_request travis_api
 travis_cron: all
 travis_push: only_build check_clean_worktree publish_tgz only_test publish_packages
-travis_pull_request: all check_clean_worktree
+travis_pull_request: only_build check_clean_worktree only_test_fast
 travis_api: all
--- build/common.mk
+++ build/common.mk
@@ -150,6 +150,7 @@ endif
 ifneq ($(SUB_PROJECTS),)
 only_build:: $(SUB_PROJECTS:%=%_only_build)
 only_test:: $(SUB_PROJECTS:%=%_only_test)
+only_test_fast:: $(SUB_PROJECTS:%=%_only_test_fast)
 default:: $(SUB_PROJECTS:%=%_default)
 all:: $(SUB_PROJECTS:%=%_all)
 ensure:: $(SUB_PROJECTS:%=%_ensure)
@@ -211,6 +212,7 @@ endif
 
 only_build:: build install
 only_test:: lint test_all
+only_test_fast:: lint test_fast
 
 # Generate targets for each sub project. This project's default and
 # all targets will depend on the sub project's targets, and the
@@ -235,6 +237,8 @@ $(SUB_PROJECTS:%=%_only_build):
 	@$(MAKE) -C ./$(@:%_only_build=%) only_build
 $(SUB_PROJECTS:%=%_only_test):
 	@$(MAKE) -C ./$(@:%_only_test=%) only_test
+$(SUB_PROJECTS:%=%_only_test_fast):
+	@$(MAKE) -C ./$(@:%_only_test_fast=%) only_test_fast
 endif
 
 # As a convinece, we provide a format target that folks can build to
