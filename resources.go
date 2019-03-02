cb50e927e21d3f5e9f610716c177c9d03b5a1697
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2019-03-01 19:22:18 -0800

    Update deps

--- resources.go
+++ resources.go
@@ -1849,7 +1849,7 @@ func Provider() tfbridge.ProviderInfo {
 		},
 		JavaScript: &tfbridge.JavaScriptInfo{
 			Dependencies: map[string]string{
-				"@pulumi/pulumi":    "^0.16.17",
+				"@pulumi/pulumi":    "^0.16.18",
 				"aws-sdk":           "^2.0.0",
 				"mime":              "^2.0.0",
 				"builtin-modules":   "3.0.0",
