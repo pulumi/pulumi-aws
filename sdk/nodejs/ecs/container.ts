233728eb677d9fa8765a6d0ae7ccc1e8c05fa4a0
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2019-03-25 10:49:13 -0700

    Update our container definition with healthcheck information.

--- sdk/nodejs/ecs/container.ts
+++ sdk/nodejs/ecs/container.ts
@@ -28,6 +28,7 @@ export interface ContainerDefinition {
     environment?: KeyValuePair[];
     essential?: boolean;
     extraHosts?: HostEntry[];
+    healthCheck?: HealthCheck;
     hostname?: string;
     image?: string;
     links?: string[];
@@ -50,6 +51,56 @@ export interface ContainerDefinition {
 export interface KeyValuePair {
     name: string;
     value: string;
+}
+
+/**
+ * The health check command and associated configuration parameters for the container. This
+ * parameter maps to HealthCheck in the Create a container section of the Docker Remote API and the
+ * HEALTHCHECK parameter of docker run.
+ *
+ * https://docs.aws.amazon.com/AmazonECS/latest/APIReference/API_HealthCheck.html
+ */
+export interface HealthCheck {
+    /**
+     * A string array representing the command that the container runs to determine if it is
+     * healthy. The string array must start with CMD to execute the command arguments directly, or
+     * CMD-SHELL to run the command with the container's default shell. For example:
+     *
+     * [ "CMD-SHELL", "curl -f http://localhost/ || exit 1" ]
+     *
+     * An exit code of 0 indicates success, and non-zero exit code indicates failure. For more
+     * information, see HealthCheck in the Create a container section of the Docker Remote API.
+     */
+    command?: string[];
+
+    /**
+     * The time period in seconds between each health check execution. You may specify between 5 and
+     * 300 seconds. The default value is 30 seconds.
+     */
+    interval?: number;
+
+    /**
+     * The number of times to retry a failed health check before the container is considered
+     * unhealthy. You may specify between 1 and 10 retries. The default value is 3.
+     */
+    retries?: number;
+
+    /**
+     * The optional grace period within which to provide containers time to bootstrap before failed
+     * health checks count towards the maximum number of retries. You may specify between 0 and 300
+     * seconds. The startPeriod is disabled by default.
+     *
+     * Note:
+     * If a health check succeeds within the startPeriod, then the container is considered healthy
+     * and any subsequent failures count toward the maximum number of retries.
+     */
+    startPeriod?: number;
+
+    /**
+     * The time period in seconds to wait for a health check to succeed before it is considered a
+     * failure. You may specify between 2 and 60 seconds. The default value is 5.
+     */
+    timeout?: number;
 }
 
 // See https://docs.aws.amazon.com/AmazonECS/latest/APIReference/API_HostEntry.html
