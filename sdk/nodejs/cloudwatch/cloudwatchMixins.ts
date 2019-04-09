88746e941e52b4fee451cccedd24c48e7515ef70
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2019-04-09 16:17:33 -0700

    more metrics

--- sdk/nodejs/cloudwatch/cloudwatchMixins.ts
+++ sdk/nodejs/cloudwatch/cloudwatchMixins.ts
@@ -16,6 +16,7 @@ import * as pulumi from "@pulumi/pulumi";
 import * as eventRule from "./eventRuleMixins";
 
 import * as sns from "../sns";
+import * as utils from "../utils";
 
 /**
  * Creates a CloudWatch event that will fire based on the specified schedule.  This will create
@@ -52,7 +53,86 @@ export function onSchedule(
  * see https://docs.aws.amazon.com/AmazonCloudWatch/latest/monitoring/cloudwatch_concepts.html#Metric
  * for more details.
  */
-export interface Metric {
+export class Metric {
+    /**
+     * The name for this metric. See docs for
+     * [supported metrics](https://docs.aws.amazon.com/AmazonCloudWatch/latest/DeveloperGuide/CW_Support_For_AWS.html).
+     */
+    metricName: pulumi.Output<string>;
+    /**
+     * The dimensions for this metric.  For the list of available dimensions see the AWS documentation
+     * [here](http://docs.aws.amazon.com/AmazonCloudWatch/latest/DeveloperGuide/CW_Support_For_AWS.html).
+     */
+    dimensions: pulumi.Output<Record<string, any> | undefined>;
+    /**
+     * The namespace for this metric. See docs for the
+     * [list of namespaces](https://docs.aws.amazon.com/AmazonCloudWatch/latest/DeveloperGuide/aws-namespaces.html).
+     * See docs for
+     * [supported metrics](https://docs.aws.amazon.com/AmazonCloudWatch/latest/DeveloperGuide/CW_Support_For_AWS.html).
+     */
+    namespace: pulumi.Output<string>;
+    /**
+     * The period in seconds over which the specified [statistic] is applied.  Must be in multiples
+     * of 60. Periods are defined in numbers of seconds, and valid values for period are 1, 5, 10,
+     * 30, or any multiple of 60. For example, to specify a period of six minutes, use 360 as the
+     * period value. You can adjust how the data is aggregated by varying the length of the period.
+     * A period can be as short as one second or as long as one day (86,400 seconds). The default
+     * value is 60 seconds.
+     *
+     * See https://docs.aws.amazon.com/AmazonCloudWatch/latest/monitoring/cloudwatch_concepts.html
+     * for more details.
+     */
+    period: pulumi.Output<number>;
+    /**
+     * The statistic to apply to the alarm's associated metric. Either of the following is
+     * supported: `SampleCount`, `Average`, `Sum`, `Minimum`, `Maximum`.
+     *
+     * Defaults to [Average] if unspecified.
+     */
+    statistic: pulumi.Output<MetricStatistic>;
+    /**
+     * The percentile statistic for the metric associated with the alarm. Specify a value between
+     * [0.0] and [100].
+     */
+    extendedStatistic: pulumi.Output<number | undefined>;
+    /**
+     * The unit for this metric.
+     *
+     * See https://docs.aws.amazon.com/AmazonCloudWatch/latest/APIReference/API_MetricDatum.html
+     */
+    unit: pulumi.Output<MetricUnit | undefined>;
+
+    /**
+     * @param resource Optional resource this is a metric for.  This is only used for parenting
+     * purposes.  i.e. if an [Alarm] is created from this [Metric], then [resource] will be used as
+     * the parent of the alarm by default.
+     */
+    constructor(args: MetricArgs, resource?: pulumi.Resource) {
+        this.metricName = pulumi.output(args.metricName);
+        this.dimensions = pulumi.output(args.dimensions);
+        this.namespace = pulumi.output(args.namespace);
+        this.period = utils.ifUndefined(args.period, 60).apply(period => {
+            // valid values for period are 1, 5, 10, 30, or any multiple of 60
+            if (period !== 1 && period !== 5 && period !== 10 && period !== 30 && period % 60 !== 0) {
+                throw new Error("Valid values for [args.period] are 1, 5, 10, 30, or any multiple of 60");
+            }
+
+            return period;
+        });
+        this.statistic = pulumi.all([args.statistic, args.extendedStatistic])
+                               .apply(([statistic, extendedStatistic]) => {
+                                   if (statistic === undefined && extendedStatistic === undefined) {
+                                       return "Average";
+                                   }
+
+                                   return statistic;
+                               });
+        this.extendedStatistic = pulumi.output(args.extendedStatistic);
+        this.unit = pulumi.output(args.unit);
+    }
+}
+
+export interface MetricArgs {
     /**
      * The name for this metric. See docs for
      * [supported metrics](https://docs.aws.amazon.com/AmazonCloudWatch/latest/DeveloperGuide/CW_Support_For_AWS.html).
