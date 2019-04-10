ddac14a2ab210f7abecb6c16cd638bafe5a85890
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2019-04-10 12:41:06 -0700

    add more metrics

--- sdk/nodejs/cloudwatch/cloudwatchMixins.ts
+++ sdk/nodejs/cloudwatch/cloudwatchMixins.ts
@@ -416,7 +416,7 @@ export type MetricUnit =
 
 // actual cloudwatch events (that cloudwatch sends to itself)
 
-export type CloudwatchMetricName =
+export type CloudwatchEventMetricName =
     "DeadLetterInvocations" | "Invocations" | "FailedInvocations" | "TriggeredRules" |
     "MatchedEvents" | "ThrottledRules";
 
@@ -436,7 +436,7 @@ export type CloudwatchMetricName =
  * CloudWatch Events metrics have one dimension:
  * 1. "RuleName": Filters the available metrics by rule name.
  */
-export function metric(metricName: CloudwatchMetricName, change: MetricChange = {} = {}) {
+export function eventMetric(metricName: CloudwatchEventMetricName, change: MetricChange = {} = {}) {
     return new Metric({
         namespace: "AWS/Events",
         name: metricName,
@@ -450,7 +450,7 @@ export function metric(metricName: CloudwatchMetricName, change: MetricChange = 
  * loop.
  */
 export function metricDeadLetterInvocations(change: MetricChange = {}) {
-    return metric("DeadLetterInvocations", { unit: "Count", ...change });
+    return eventMetric("DeadLetterInvocations", { unit: "Count", ...change });
 }
 
 /**
@@ -461,7 +461,7 @@ export function metricDeadLetterInvocations(change: MetricChange = {}) {
  * Note: CloudWatch Events only sends this metric to CloudWatch if it has a non-zero value.
  */
 export function metricInvocations(change: MetricChange = {}) {
-    return metric("Invocations", { unit: "Count", ...change });
+    return eventMetric("Invocations", { unit: "Count", ...change });
 }
 
 /**
@@ -470,26 +470,103 @@ export function metricInvocations(change: MetricChange = {}) {
  * invocations that are counted in DeadLetterInvocations.
  */
 export function metricFailedInvocations(change: MetricChange = {}) {
-    return metric("FailedInvocations", { unit: "Count", ...change });
+    return eventMetric("FailedInvocations", { unit: "Count", ...change });
 }
 
 /**
  * Measures the number of triggered rules that matched with any event.
  */
 export function metricTriggeredRules(change: MetricChange = {}) {
-    return metric("TriggeredRules", { unit: "Count", ...change });
+    return eventMetric("TriggeredRules", { unit: "Count", ...change });
 }
 
 /**
  * Measures the number of events that matched with any rule.
  */
 export function metricMatchedEvents(change: MetricChange = {}) {
-    return metric("MatchedEvents", { unit: "Count", ...change });
+    return eventMetric("MatchedEvents", { unit: "Count", ...change });
 }
 
 /**
  * Measures the number of triggered rules that are being throttled.
  */
 export function metricThrottledRules(change: MetricChange = {}) {
-    return metric("ThrottledRules", { unit: "Count", ...change });
+    return eventMetric("ThrottledRules", { unit: "Count", ...change });
+}
+
+export type CloudwatchLogMetricName =
+    "IncomingBytes" | "IncomingLogEvents" | "ForwardedBytes" | "ForwardedLogEvents" |
+    "DeliveryErrors" | "DeliveryThrottling";
+
+/**
+ * CloudWatch Logs sends metrics to Amazon CloudWatch every minute.
+ *
+ * Creates an AWS/Logs metric with the requested [metricName]. See
+ * https://docs.aws.amazon.com/AmazonCloudWatch/latest/logs/CloudWatch-Logs-Monitoring-CloudWatch-Metrics.html
+ * for list of all metric-names.
+ *
+ * Note, individual metrics can easily be obtained without supplying the name using the other
+ * [metricXXX] functions.
+ *
+ * The dimensions that you can use with CloudWatch Logs metrics are:
+ * 1. "LogGroupName": The name of the CloudWatch Logs log group for which to display metrics.
+ * 2. "DestinationType": The subscription destination for the CloudWatch Logs data, which can be AWS
+ *    Lambda, Amazon Kinesis Data Streams, or Amazon Kinesis Data Firehose.
+ * 3. "FilterName": The name of the subscription filter that is forwarding data from the log group
+ *    to the destination. The subscription filter name is automatically converted by CloudWatch to
+ *    ASCII and any unsupported characters get replaced with a question mark (?).
+ */
+export function logMetric(metricName: CloudwatchLogMetricName, change: MetricChange = {} = {}) {
+    return new Metric({
+        namespace: "AWS/Logs",
+        name: metricName,
+        ...change,
+    });
+}
+
+/**
+ * The volume of log events in uncompressed bytes uploaded to CloudWatch Logs. When used with the
+ * LogGroupName dimension, this is the volume of log events in uncompressed bytes uploaded to the
+ * log group.
+ */
+export function metricIncomingBytes(change: MetricChange = {}) {
+    return logMetric("IncomingBytes", { statistic: "Sum", unit: "Bytes", ...change });
+}
+
+/**
+ * The number of log events uploaded to CloudWatch Logs. When used with the LogGroupName dimension,
+ * this is the number of log events uploaded to the log group.
+ */
+export function metricIncomingLogEvents(change: MetricChange = {}) {
+    return logMetric("IncomingLogEvents", { statistic: "Sum", unit: "None", ...change });
+}
+
+/**
+ * The volume of log events in compressed bytes forwarded to the subscription destination.
+ */
+export function metricForwardedBytes(change: MetricChange = {}) {
+    return logMetric("ForwardedBytes", { statistic: "Sum", unit: "Bytes", ...change });
+}
+
+/**
+ * The number of log events forwarded to the subscription destination.
+ */
+export function metricForwardedLogEvents(change: MetricChange = {}) {
+    return logMetric("ForwardedLogEvents", { statistic: "Sum", unit: "None", ...change });
+}
+
+/**
+ * The number of log events for which CloudWatch Logs received an error when forwarding data to the
+ * subscription destination.
+ */
+export function metricDeliveryErrors(change: MetricChange = {}) {
+    return logMetric("DeliveryErrors", { statistic: "Sum", unit: "None", ...change });
+}
+
+/**
+ * The number of log events for which CloudWatch Logs was throttled when forwarding data to the
+ * subscription destination.
+ */
+export function metricDeliveryThrottling(change: MetricChange = {}) {
+    return logMetric("DeliveryThrottling", { statistic: "Sum", unit: "None", ...change });
 }
