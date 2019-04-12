6af98e73f35e7f50699b21877c400096a72c7b94
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2019-04-12 14:46:03 -0700

    Add instance metrics for sns.Topics

--- sdk/nodejs/sns/metrics.ts
+++ sdk/nodejs/sns/metrics.ts
@@ -16,6 +16,8 @@ import * as pulumi from "@pulumi/pulumi";
 
 import * as cloudwatch from "../cloudwatch";
 
+import { Topic } from "./topic";
+
 export module metrics {
     export type SnsMetricName =
         "NumberOfMessagesPublished" | "NumberOfNotificationsDelivered" |
@@ -174,3 +176,131 @@ export module metrics {
         return metric("SMSSuccessRate", { unit: "Count", ...change });
     }
 }
+
+declare module "./topic" {
+    interface Topic {
+        /**
+         * Direct access to create metrics for this specific [sns.Topic].
+         */
+        metrics: {
+            /**
+             * The number of messages published to your Amazon SNS topics.
+             *
+             * Units: Count
+             *
+             * Valid Statistics: Sum
+             */
+            numberOfMessagesPublished(change?: cloudwatch.MetricChange): cloudwatch.Metric;
+
+            /**
+             * The number of messages successfully delivered from your Amazon SNS topics to subscribing
+             * endpoints.
+             *
+             * For a delivery attempt to succeed, the endpoint's subscription must accept the message. A
+             * subscription accepts a message if a.) it lacks a filter policy or b.) its filter policy
+             * includes attributes that match those assigned to the message. If the subscription rejects the
+             * message, the delivery attempt isn't counted for this metric.
+             *
+             * Units: Count
+             *
+             * Valid Statistics: Sum
+             */
+            numberOfNotificationsDelivered(change?: cloudwatch.MetricChange): cloudwatch.Metric;
+
+            /**
+             * The number of messages that Amazon SNS failed to deliver.
+             *
+             * For Amazon SQS, email, SMS, or mobile push endpoints, the metric increments by 1 when Amazon
+             * SNS stops attempting message deliveries. For HTTP or HTTPS endpoints, the metric includes
+             * every failed delivery attempt, including retries that follow the initial attempt. For all
+             * other endpoints, the count increases by 1 when the message fails to deliver (regardless of
+             * the number of attempts).
+             *
+             * This metric does not include messages that were rejected by subscription filter policies.
+             *
+             * Units: Count
+             *
+             * Valid Statistics: Sum, Average
+             */
+            numberOfNotificationsFailed(change?: cloudwatch.MetricChange): cloudwatch.Metric;
+
+            /**
+             * The number of messages that were rejected by subscription filter policies because the
+             * messages have no attributes.
+             *
+             * Units: Count
+             *
+             * Valid Statistics: Sum, Average
+             */
+            numberOfNotificationsFilteredOut_NoMessageAttributes(change?: cloudwatch.MetricChange): cloudwatch.Metric;
+
+            /**
+             * The number of messages that were rejected by subscription filter policies because the
+             * messages' attributes are invalid – for example, because the attribute JSON is incorrectly
+             * formatted.
+             *
+             * Units: Count
+             *
+             * Valid Statistics: Sum, Average
+             */
+            numberOfNotificationsFilteredOut_InvalidAttributes(change?: cloudwatch.MetricChange): cloudwatch.Metric;
+
+            /**
+             * The number of database connections in use.
+             *
+             * Units: Bytes
+             *
+             * Valid Statistics: Minimum, Maximum, Average and Count
+             */
+            publishSize(change?: cloudwatch.MetricChange): cloudwatch.Metric;
+
+            /**
+             * The charges you have accrued since the start of the current calendar month for sending SMS
+             * messages.
+             *
+             * You can set an alarm for this metric to know when your month-to-date charges are close to the
+             * monthly SMS spend limit for your account. When Amazon SNS determines that sending an SMS
+             * message would incur a cost that exceeds this limit, it stops publishing SMS messages within
+             * minutes.
+             *
+             * Valid Statistics: Maximum
+             */
+            smsMonthToDateSpentUSD(change?: cloudwatch.MetricChange): cloudwatch.Metric;
+
+            /**
+             * The rate of successful SMS message deliveries.
+             *
+             * Units: Count
+             *
+             * Valid Statistics: Sum, Average, Data Samples
+             */
+            smsSuccessRate(change?: cloudwatch.MetricChange): cloudwatch.Metric;
+        }
+    }
+}
+
+// All instance metrics just make a normal AWS/SNS metric, except with the TopicName
+// dimension set to this Topic's name.
+
+function getMetric(
+    topic: Topic, change: cloudwatch.MetricChange,
+    moduleFunction: (change: cloudwatch.MetricChange) => cloudwatch.Metric) {
+
+    return moduleFunction({ dimensions: { TopicName: topic.name } }).with(change);
+}
+
+Object.defineProperty(Topic.prototype, "metrics", {
+    get: function (this: Topic) {
+        const _this = this;
+        return {
+            numberOfMessagesPublished: (change: cloudwatch.MetricChange) => getMetric(_this, change, metrics.numberOfMessagesPublished),
+            numberOfNotificationsDelivered: (change: cloudwatch.MetricChange) => getMetric(_this, change, metrics.numberOfNotificationsDelivered),
+            numberOfNotificationsFailed: (change: cloudwatch.MetricChange) => getMetric(_this, change, metrics.numberOfNotificationsFailed),
+            numberOfNotificationsFilteredOut_NoMessageAttributes: (change: cloudwatch.MetricChange) => getMetric(_this, change, metrics.numberOfNotificationsFilteredOut_NoMessageAttributes),
+            numberOfNotificationsFilteredOut_InvalidAttributes: (change: cloudwatch.MetricChange) => getMetric(_this, change, metrics.numberOfNotificationsFilteredOut_InvalidAttributes),
+            publishSize: (change: cloudwatch.MetricChange) => getMetric(_this, change, metrics.publishSize),
+            smsMonthToDateSpentUSD: (change: cloudwatch.MetricChange) => getMetric(_this, change, metrics.smsMonthToDateSpentUSD),
+            smsSuccessRate: (change: cloudwatch.MetricChange) => getMetric(_this, change, metrics.smsSuccessRate),
+        }
+    }
+});
