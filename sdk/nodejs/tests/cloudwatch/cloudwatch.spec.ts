176c766d11ed175f332039128228e14df77163d5
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2019-04-14 14:59:05 -0700

    tests

--- sdk/nodejs/tests/cloudwatch/cloudwatch.spec.ts
+++ sdk/nodejs/tests/cloudwatch/cloudwatch.spec.ts
@@ -239,5 +239,184 @@ describe("dashboard", () => {
 }`);
             });
         });
+
+        describe("vertical", () => {
+            it("two widgets", async () => {
+                const json = await bodyJson(
+                    new ColumnWidget(new TextWidget("hello"), new TextWidget("world")));
+                assert.equal(json, `{
+    "widgets": [
+        {
+            "x": 0,
+            "y": 0,
+            "width": 6,
+            "height": 6,
+            "type": "text",
+            "properties": {
+                "markdown": "hello"
+            }
+        },
+        {
+            "x": 0,
+            "y": 6,
+            "width": 6,
+            "height": 6,
+            "type": "text",
+            "properties": {
+                "markdown": "world"
+            }
+        }
+    ]
+}`);
+            });
+            it("multiple widgets without wrapping", async () => {
+                const json = await bodyJson(new ColumnWidget(
+                    new TextWidget("hello"),
+                    new TextWidget("world"),
+                    new TextWidget({ markdown: "goodnight", height: 1, width: 18 }),
+                    new TextWidget({ markdown: "moon", height: 3, width: 6 }),
+                    new TextWidget({ markdown: "!", height: 5, width: 1 })));
+                assert.equal(json, `{
+    "widgets": [
+        {
+            "x": 0,
+            "y": 0,
+            "width": 6,
+            "height": 6,
+            "type": "text",
+            "properties": {
+                "markdown": "hello"
+            }
+        },
+        {
+            "x": 0,
+            "y": 6,
+            "width": 6,
+            "height": 6,
+            "type": "text",
+            "properties": {
+                "markdown": "world"
+            }
+        },
+        {
+            "x": 0,
+            "y": 12,
+            "width": 18,
+            "height": 1,
+            "type": "text",
+            "properties": {
+                "markdown": "goodnight"
+            }
+        },
+        {
+            "x": 0,
+            "y": 13,
+            "width": 6,
+            "height": 3,
+            "type": "text",
+            "properties": {
+                "markdown": "moon"
+            }
+        },
+        {
+            "x": 0,
+            "y": 16,
+            "width": 1,
+            "height": 5,
+            "type": "text",
+            "properties": {
+                "markdown": "!"
+            }
+        }
+    ]
+}`);
+            });
+            it("multiple columns without wrapping", async () => {
+                const json = await bodyJson(
+                    new ColumnWidget(
+                        new TextWidget({ markdown: "hello", height: 4 }),
+                        new TextWidget({ markdown: "world", height: 3 })),
+                    new ColumnWidget(
+                        new TextWidget({ markdown: "goodnight", height: 1, width: 10 }),
+                        new TextWidget({ markdown: "moon", height: 3, width: 10 }),
+                        new TextWidget({ markdown: "!", height: 5, width: 10 })),
+                    new ColumnWidget(new TextWidget("byebye")),
+                    new ColumnWidget(new TextWidget("byebye2")));
+                assert.equal(json, `{
+    "widgets": [
+        {
+            "x": 0,
+            "y": 0,
+            "width": 6,
+            "height": 4,
+            "type": "text",
+            "properties": {
+                "markdown": "hello"
+            }
+        },
+        {
+            "x": 0,
+            "y": 4,
+            "width": 6,
+            "height": 3,
+            "type": "text",
+            "properties": {
+                "markdown": "world"
+            }
+        },
+        {
+            "x": 6,
+            "y": 0,
+            "width": 10,
+            "height": 1,
+            "type": "text",
+            "properties": {
+                "markdown": "goodnight"
+            }
+        },
+        {
+            "x": 6,
+            "y": 1,
+            "width": 10,
+            "height": 3,
+            "type": "text",
+            "properties": {
+                "markdown": "moon"
+            }
+        },
+        {
+            "x": 6,
+            "y": 4,
+            "width": 10,
+            "height": 5,
+            "type": "text",
+            "properties": {
+                "markdown": "!"
+            }
+        },
+        {
+            "x": 16,
+            "y": 0,
+            "width": 6,
+            "height": 6,
+            "type": "text",
+            "properties": {
+                "markdown": "byebye"
+            }
+        },
+        {
+            "x": 0,
+            "y": 9,
+            "width": 6,
+            "height": 6,
+            "type": "text",
+            "properties": {
+                "markdown": "byebye2"
+            }
+        }
+    ]
+}`);
+            });
+        })
     });
 });
