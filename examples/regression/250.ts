9b03331ebe3d5d76eb9c65ec81c00e1e87d75f49
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2019-06-19 09:56:58 -0700

    Update tests

--- examples/regression/250.ts
+++ examples/regression/250.ts
@@ -1,8 +1,13 @@
+const pulumi = require("@pulumi/pulumi");
 const aws = require("@pulumi/aws");
-const apiKey = new aws.apigateway.ApiKey("key", {});
-const usagePlan = new aws.apigateway.UsagePlan("plan", {});
+
+const config = new pulumi.Config("aws");
+const providerOpts = { provider: new aws.Provider("prov", { region: config.require("envRegion") }) };
+
+const apiKey = new aws.apigateway.ApiKey("key", {}, providerOpts);
+const usagePlan = new aws.apigateway.UsagePlan("plan", {}, providerOpts);
 exports.usagePlanKey = new aws.apigateway.UsagePlanKey('MyUsagePlanKey',{
     keyId: apiKey.id,
     keyType: 'API_KEY',
     usagePlanId: usagePlan.id
-});
+}, providerOpts);
