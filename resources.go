fa95e39c5706084b7cd9a6826a39268a863e27c8
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2019-06-24 11:32:26 -0700

    Update referenced version

--- resources.go
+++ resources.go
@@ -1950,7 +1950,7 @@ func Provider() tfbridge.ProviderInfo {
 		},
 		JavaScript: &tfbridge.JavaScriptInfo{
 			Dependencies: map[string]string{
-				"@pulumi/pulumi":    "^0.17.18",
+				"@pulumi/pulumi":    "^0.17.20",
 				"aws-sdk":           "^2.0.0",
 				"mime":              "^2.0.0",
 				"builtin-modules":   "3.0.0",
