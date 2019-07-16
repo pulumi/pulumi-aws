929b4b078c3271acf795c75e6ee47adaabdc37a7
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2019-07-16 03:19:28 -0700

    tweaks

--- sdk/nodejs/athena/workgroup.ts
+++ sdk/nodejs/athena/workgroup.ts
@@ -6,26 +6,25 @@ import * as utilities from "../utilities";
 
 /**
  * Provides an Athena Workgroup.
- * 
+ *
  * ## Example Usage
- * 
+ *
  * ```typescript
  * import * as pulumi from "@pulumi/pulumi";
  * import * as aws from "@pulumi/aws";
- * 
+ *
  * const example = new aws.athena.Workgroup("example", {
- *     configuration: [{
+ *     configuration: {
  *         enforceWorkgroupConfiguration: true,
  *         publishCloudwatchMetricsEnabled: true,
- *         resultConfiguration: [{
- *             encryptionConfiguration: [{
+ *         resultConfiguration: {
+ *             encryptionConfiguration: {
  *                 encryptionOption: "SSE_KMS",
  *                 kmsKeyArn: aws_kms_key_example.arn,
- *             }],
+ *             },
  *             outputLocation: "s3://{aws_s3_bucket.example.bucket}/output/",
- *         }],
- *     }],
- *     name: "example",
+ *         },
+ *     },
  * });
  * ```
  *
--- sdk/nodejs/datapipeline/pipeline.ts
+++ sdk/nodejs/datapipeline/pipeline.ts
@@ -6,16 +6,14 @@ import * as utilities from "../utilities";
 
 /**
  * Provides a Data Pipeline resource.
- * 
+ *
  * ## Example Usage
- * 
+ *
  * ```typescript
  * import * as pulumi from "@pulumi/pulumi";
  * import * as aws from "@pulumi/aws";
- * 
- * const defaultPipeline = new aws.datapipeline.Pipeline("default", {
- *     name: "tf-pipeline-default",
- * });
+ *
+ * const defaultPipeline = new aws.datapipeline.Pipeline("default", {});
  * ```
  *
  * > This content is derived from https://github.com/terraform-providers/terraform-provider-aws/blob/master/website/docs/r/datapipeline_pipeline.html.markdown.
