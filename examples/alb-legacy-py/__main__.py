1d286f9dcce5f5a3debd8a31691ca5070eac1445
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2019-07-22 15:15:19 -0700

    Zone names

--- examples/alb-legacy-py/__main__.py
+++ examples/alb-legacy-py/__main__.py
@@ -1,7 +1,7 @@
 # Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import pulumi
-from pulumi_aws import applicationloadbalancing, get_availability_zones, ec2
+from pulumi_aws import applicationloadbalancing, get_availability_zones, ec2, Output
 
 vpc = ec2.Vpc('test',
     cidr_block="10.10.0.0/16",
@@ -18,15 +18,19 @@ route_table = ec2.RouteTable('test',
         "gatewayId": internet_gateway.id
     }])
 
+zones = Output.from_input(get_availability_zones())
+zone_names = zones.apply(
+    lambda zs: zs.names)
+
 subnet0 = ec2.Subnet("test0",
     vpc_id=vpc.id,
-    availability_zone=get_availability_zones().names[0],
+    availability_zone=zone_names.apply(lambda names: names[0]),
     cidr_block="10.10.0.0/24",
     map_public_ip_on_launch=True)
 
 subnet1 = ec2.Subnet("test1",
     vpc_id=vpc.id,
-    availability_zone=get_availability_zones().names[1],
+    availability_zone=zone_names.apply(lambda names: names[1]),
     cidr_block="10.10.1.0/24",
     map_public_ip_on_launch=True)
 
