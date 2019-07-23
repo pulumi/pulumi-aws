15721824da6b5355a81ece180ed5bedb7a40ec7a
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2019-07-23 11:21:01 -0700

    Other attempt

--- sdk/python/pulumi_aws/extra_imports.py
+++ sdk/python/pulumi_aws/extra_imports.py
@@ -1,6 +1,4 @@
-import importlib
 # Make subpackages available:
-__all__ = ['applicationloadbalancing']
-for pkg in __all__:
-    if pkg != 'config':
-        importlib.import_module(f'{__name__}.{pkg}')
+
+# Expose legacy library which moved to elbv2
+from . import applicationloadbalancing
