197c3178837b1013448ddcba6db8653077fa17a9
Pat Gavlin <pat@pulumi.com>
2019-11-05 15:57:45 -0800

    Change the .NET version constraint.

--- resources.go
+++ resources.go
@@ -2206,7 +2206,7 @@ func Provider() tfbridge.ProviderInfo {
 		},
 		CSharp: &tfbridge.CSharpInfo{
 			PackageReferences: map[string]string{
-				"Pulumi":                       "1.5.0-*",
+				"Pulumi":                       "1-*",
 				"System.Collections.Immutable": "1.6.0",
 			},
 		},
