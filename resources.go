136a76370f72a7f4e05e63c1610dc1bc0611738c
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2019-12-02 15:45:31 -0800

    dedeasync version

--- resources.go
+++ resources.go
@@ -2145,7 +2145,7 @@ func Provider() tfbridge.ProviderInfo {
 		},
 		JavaScript: &tfbridge.JavaScriptInfo{
 			Dependencies: map[string]string{
-				"@pulumi/pulumi":    "^1.0.0",
+				"@pulumi/pulumi":    "feature-dedeasync",
 				"aws-sdk":           "^2.0.0",
 				"mime":              "^2.0.0",
 				"builtin-modules":   "3.0.0",
