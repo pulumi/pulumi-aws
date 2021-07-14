d67b8a72a6cef1c0333ac2469d2d7bee0af3cb33
Pat Gavlin <pat@pulumi.com>
2021-07-14 16:00:38 -0700

    Update pulumi + pulumi-terraform-bridge

--- sdk/python/pulumi_aws/config/vars.py
+++ sdk/python/pulumi_aws/config/vars.py
@@ -9,117 +9,138 @@ from typing import Any, Mapping, Optional, Sequence, Union, overload
 from .. import _utilities
 from . import outputs
 
-__all__ = [
-    'access_key',
-    'allowed_account_ids',
-    'assume_role',
-    'default_tags',
-    'endpoints',
-    'forbidden_account_ids',
-    'ignore_tags',
-    'insecure',
-    'max_retries',
-    'profile',
-    'region',
-    's3_force_path_style',
-    'secret_key',
-    'shared_credentials_file',
-    'skip_credentials_validation',
-    'skip_get_ec2_platforms',
-    'skip_metadata_api_check',
-    'skip_region_validation',
-    'skip_requesting_account_id',
-    'token',
-]
+import types
 
 __config__ = pulumi.Config('aws')
 
-access_key = __config__.get('accessKey')
-"""
-The access key for API operations. You can retrieve this from the 'Security & Credentials' section of the AWS console.
-"""
 
-allowed_account_ids = __config__.get('allowedAccountIds')
+class _ExportableConfig(types.ModuleType):
+    @property
+    def access_key(self) -> Optional[str]:
+        """
+        The access key for API operations. You can retrieve this from the 'Security & Credentials' section of the AWS console.
+        """
+        return __config__.get('accessKey')
 
-assume_role = __config__.get('assumeRole')
+    @property
+    def allowed_account_ids(self) -> Optional[str]:
+        return __config__.get('allowedAccountIds')
 
-default_tags = __config__.get('defaultTags')
-"""
-Configuration block with settings to default resource tags across all resources.
-"""
+    @property
+    def assume_role(self) -> Optional[str]:
+        return __config__.get('assumeRole')
 
-endpoints = __config__.get('endpoints')
+    @property
+    def default_tags(self) -> Optional[str]:
+        """
+        Configuration block with settings to default resource tags across all resources.
+        """
+        return __config__.get('defaultTags')
 
-forbidden_account_ids = __config__.get('forbiddenAccountIds')
+    @property
+    def endpoints(self) -> Optional[str]:
+        return __config__.get('endpoints')
 
-ignore_tags = __config__.get('ignoreTags')
-"""
-Configuration block with settings to ignore resource tags across all resources.
-"""
+    @property
+    def forbidden_account_ids(self) -> Optional[str]:
+        return __config__.get('forbiddenAccountIds')
 
-insecure = __config__.get('insecure')
-"""
-Explicitly allow the provider to perform "insecure" SSL requests. If omitted,default value is `false`
-"""
+    @property
+    def ignore_tags(self) -> Optional[str]:
+        """
+        Configuration block with settings to ignore resource tags across all resources.
+        """
+        return __config__.get('ignoreTags')
 
-max_retries = __config__.get('maxRetries')
-"""
-The maximum number of times an AWS API request is being executed. If the API request still fails, an error is thrown.
-"""
+    @property
+    def insecure(self) -> Optional[str]:
+        """
+        Explicitly allow the provider to perform "insecure" SSL requests. If omitted,default value is `false`
+        """
+        return __config__.get('insecure')
 
-profile = __config__.get('profile') or _utilities.get_env('AWS_PROFILE')
-"""
-The profile for API operations. If not set, the default profile created with `aws configure` will be used.
-"""
+    @property
+    def max_retries(self) -> Optional[str]:
+        """
+        The maximum number of times an AWS API request is being executed. If the API request still fails, an error is thrown.
+        """
+        return __config__.get('maxRetries')
 
-region = __config__.get('region') or _utilities.get_env('AWS_REGION', 'AWS_DEFAULT_REGION')
-"""
-The region where AWS operations will take place. Examples are us-east-1, us-west-2, etc.
-"""
+    @property
+    def profile(self) -> Optional[str]:
+        """
+        The profile for API operations. If not set, the default profile created with `aws configure` will be used.
+        """
+        return __config__.get('profile') or _utilities.get_env('AWS_PROFILE')
 
-s3_force_path_style = __config__.get('s3ForcePathStyle')
-"""
-Set this to true to force the request to use path-style addressing, i.e., http://s3.amazonaws.com/BUCKET/KEY. By
-default, the S3 client will use virtual hosted bucket addressing when possible (http://BUCKET.s3.amazonaws.com/KEY).
-Specific to the Amazon S3 service.
-"""
+    @property
+    def region(self) -> str:
+        """
+        The region where AWS operations will take place. Examples are us-east-1, us-west-2, etc.
+        """
+        return __config__.get('region') or _utilities.get_env('AWS_REGION', 'AWS_DEFAULT_REGION')
 
-secret_key = __config__.get('secretKey')
-"""
-The secret key for API operations. You can retrieve this from the 'Security & Credentials' section of the AWS console.
-"""
+    @property
+    def s3_force_path_style(self) -> Optional[str]:
+        """
+        Set this to true to force the request to use path-style addressing, i.e., http://s3.amazonaws.com/BUCKET/KEY. By
+        default, the S3 client will use virtual hosted bucket addressing when possible (http://BUCKET.s3.amazonaws.com/KEY).
+        Specific to the Amazon S3 service.
+        """
+        return __config__.get('s3ForcePathStyle')
 
-shared_credentials_file = __config__.get('sharedCredentialsFile')
-"""
-The path to the shared credentials file. If not set this defaults to ~/.aws/credentials.
-"""
+    @property
+    def secret_key(self) -> Optional[str]:
+        """
+        The secret key for API operations. You can retrieve this from the 'Security & Credentials' section of the AWS console.
+        """
+        return __config__.get('secretKey')
 
-skip_credentials_validation = __config__.get('skipCredentialsValidation') or True
-"""
-Skip the credentials validation via STS API. Used for AWS API implementations that do not have STS
-available/implemented.
-"""
+    @property
+    def shared_credentials_file(self) -> Optional[str]:
+        """
+        The path to the shared credentials file. If not set this defaults to ~/.aws/credentials.
+        """
+        return __config__.get('sharedCredentialsFile')
 
-skip_get_ec2_platforms = __config__.get('skipGetEc2Platforms') or True
-"""
-Skip getting the supported EC2 platforms. Used by users that don't have ec2:DescribeAccountAttributes permissions.
-"""
+    @property
+    def skip_credentials_validation(self) -> Optional[str]:
+        """
+        Skip the credentials validation via STS API. Used for AWS API implementations that do not have STS
+        available/implemented.
+        """
+        return __config__.get('skipCredentialsValidation') or True
 
-skip_metadata_api_check = __config__.get('skipMetadataApiCheck') or True
+    @property
+    def skip_get_ec2_platforms(self) -> Optional[str]:
+        """
+        Skip getting the supported EC2 platforms. Used by users that don't have ec2:DescribeAccountAttributes permissions.
+        """
+        return __config__.get('skipGetEc2Platforms') or True
 
-skip_region_validation = __config__.get('skipRegionValidation') or True
-"""
-Skip static validation of region name. Used by users of alternative AWS-like APIs or users w/ access to regions that are
-not public (yet).
-"""
+    @property
+    def skip_metadata_api_check(self) -> Optional[str]:
+        return __config__.get('skipMetadataApiCheck') or True
 
-skip_requesting_account_id = __config__.get('skipRequestingAccountId')
-"""
-Skip requesting the account ID. Used for AWS API implementations that do not have IAM/STS API and/or metadata API.
-"""
+    @property
+    def skip_region_validation(self) -> Optional[str]:
+        """
+        Skip static validation of region name. Used by users of alternative AWS-like APIs or users w/ access to regions that are
+        not public (yet).
+        """
+        return __config__.get('skipRegionValidation') or True
 
-token = __config__.get('token')
-"""
-session token. A session token is only required if you are using temporary security credentials.
-"""
+    @property
+    def skip_requesting_account_id(self) -> Optional[str]:
+        """
+        Skip requesting the account ID. Used for AWS API implementations that do not have IAM/STS API and/or metadata API.
+        """
+        return __config__.get('skipRequestingAccountId')
+
+    @property
+    def token(self) -> Optional[str]:
+        """
+        session token. A session token is only required if you are using temporary security credentials.
+        """
+        return __config__.get('token')
 
