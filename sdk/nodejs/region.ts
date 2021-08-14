81b7badcf1983d3ecf91fb094565858259970ef0
Josh Graham <jgraham@securecodewarrior.com>
2021-08-14 15:14:39 +1000

    Add city/area names to Region enum. Add ap-northeast-3.
     - Add APNortheast3 (Osaka) - this may fix #1391 https://github.com/pulumi/pulumi-aws/issues/1391
     - Allow code to also use city / area name as a Region enum.
     - Fix deprecated constants messages (some targets were wrong in comment).
     - Use Region enums in deprecated region constants.
     - Reorder to alphabetical entries (almost).

--- sdk/nodejs/region.ts
+++ sdk/nodejs/region.ts
@@ -20,50 +20,50 @@
 import {Region} from ".";
 
 /** The AFSouth1Region constant is deprecated, use Region.AFSouth1 instead. */
-export let AFSouth1Region: Region     = "af-south-1";
-/** The APEast1Region constant is deprecated, use Region.AFEast1 instead. */
-export let APEast1Region: Region      = "ap-east-1";
-/** The APNortheast1Region constant is deprecated, use Region.AFNortheast1 instead. */
-export let APNortheast1Region: Region = "ap-northeast-1";
+export let AFSouth1Region: Region     = Region.AFSouth1;
+/** The APEast1Region constant is deprecated, use Region.APEast1 instead. */
+export let APEast1Region: Region      = Region.APEast1;
+/** The APNortheast1Region constant is deprecated, use Region.APNortheast1 instead. */
+export let APNortheast1Region: Region = Region.APNortheast1;
 /** The APNortheast2Region constant is deprecated, use Region.APNortheast2 instead. */
-export let APNortheast2Region: Region = "ap-northeast-2";
+export let APNortheast2Region: Region = Region.APNortheast2;
 /** The APSouth1Region constant is deprecated, use Region.APSouth1 instead. */
-export let APSouth1Region: Region     = "ap-south-1";
-/** The APSouthEast2Region constant is deprecated, use Region.APSoutheast2 instead. */
-export let APSouthEast2Region: Region = "ap-southeast-2";
+export let APSouth1Region: Region     = Region.APSouth1;
 /** The APSoutheast1Region constant is deprecated, use Region.APSoutheast1 instead. */
-export let APSoutheast1Region: Region = "ap-southeast-1";
-/** The CACentralRegion constant is deprecated, use Region.CACentral1 instead. */
-export let CACentralRegion: Region    = "ca-central-1";
+export let APSoutheast1Region: Region = Region.APSoutheast1;
+/** The APSouthEast2Region constant is deprecated, use Region.APSoutheast2 instead. */
+export let APSouthEast2Region: Region = Region.APSoutheast2;
+/** The CACentralRegion constant is deprecated, use Region.CACentral instead. */
+export let CACentralRegion: Region    = Region.CACentral;
 /** The CNNorth1Region constant is deprecated, use Region.CNNorth1 instead. */
-export let CNNorth1Region: Region     = "cn-north-1";
+export let CNNorth1Region: Region     = Region.CNNorth1;
 /** The CNNorthWest1Region constant is deprecated, use Region.CNNorthwest1 instead. */
-export let CNNorthWest1Region: Region = "cn-northwest-1";
+export let CNNorthWest1Region: Region = Region.CNNorthwest1;
 /** The EUCentral1Region constant is deprecated, use Region.EUCentral1 instead. */
-export let EUCentral1Region: Region   = "eu-central-1";
+export let EUCentral1Region: Region   = Region.EUCentral1;
 /** The EUNorth1Region constant is deprecated, use Region.EUNorth1 instead. */
-export let EUNorth1Region: Region     = "eu-north-1";
+export let EUNorth1Region: Region     = Region.EUNorth1;
 /** The EUWest1Region constant is deprecated, use Region.EUWest1 instead. */
-export let EUWest1Region: Region      = "eu-west-1";
+export let EUWest1Region: Region      = Region.EUWest1;
 /** The EUWest2Region constant is deprecated, use Region.EUWest2 instead. */
-export let EUWest2Region: Region      = "eu-west-2";
+export let EUWest2Region: Region      = Region.EUWest2;
 /** The EUWest3Region constant is deprecated, use Region.EUWest3 instead. */
-export let EUWest3Region: Region      = "eu-west-3";
+export let EUWest3Region: Region      = Region.EUWest3;
 /** The EUSouth1Region constant is deprecated, use Region.EUSouth1 instead. */
-export let EUSouth1Region: Region     = "eu-south-1";
+export let EUSouth1Region: Region     = Region.EUSouth1;
 /** The MESouth1Region constant is deprecated, use Region.MESouth1 instead. */
-export let MESouth1Region: Region     = "me-south-1";
+export let MESouth1Region: Region     = Region.MESouth1;
 /** The SAEast1Region constant is deprecated, use Region.SAEast1 instead. */
-export let SAEast1Region: Region      = "sa-east-1";
+export let SAEast1Region: Region      = Region.SAEast1;
 /** The USGovEast1Region constant is deprecated, use Region.USGovEast1 instead. */
-export let USGovEast1Region: Region   = "us-gov-east-1";
+export let USGovEast1Region: Region   = Region.USGovEast1;
 /** The USGovWest1Region constant is deprecated, use Region.USGovWest1 instead. */
-export let USGovWest1Region: Region   = "us-gov-west-1";
+export let USGovWest1Region: Region   = Region.USGovWest1;
 /** The USEast1Region constant is deprecated, use Region.USEast1 instead. */
-export let USEast1Region: Region      = "us-east-1";
+export let USEast1Region: Region      = Region.USEast1;
 /** The USEast2Region constant is deprecated, use Region.USEast2 instead. */
-export let USEast2Region: Region      = "us-east-2";
+export let USEast2Region: Region      = Region.USEast2;
 /** The USWest1Region constant is deprecated, use Region.USWest1 instead. */
-export let USWest1Region: Region      = "us-west-1";
+export let USWest1Region: Region      = Region.USWest1;
 /** The USWest2Region constant is deprecated, use Region.USWest2 instead. */
-export let USWest2Region: Region      = "us-west-2";
+export let USWest2Region: Region      = Region.USWest2;
