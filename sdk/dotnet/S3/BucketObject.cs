d0db1cefbf66550d5dbcfaf6fa1bb43ce4b9f0e3
stack72 <public@paulstack.co.uk>
2022-04-21 12:01:11 +0100

    aws.s3.BucketObject: Remove the Deprecation Warnings
    Fixes: #1911
    FixeS: #1910
    
    These warnings were from upstream and not someething we care about
    disclosing to the user

--- sdk/dotnet/S3/BucketObject.cs
+++ sdk/dotnet/S3/BucketObject.cs
@@ -10,6 +10,8 @@ using Pulumi.Serialization;
 namespace Pulumi.Aws.S3
 {
     /// <summary>
+    /// Provides an S3 object resource.
+    /// 
     /// ## Example Usage
     /// ### Encrypting with KMS Key
     /// 
