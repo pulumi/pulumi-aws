06905821209afb26cb8454e545d6a45da49e04e6
Daniel Bradley <daniel@pulumi.com>
2022-12-05 13:45:12 +0000

    Document apply script
    - Make it a little less noisy

--- patching/apply.sh
+++ patching/apply.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
-yarn install --frozen-lockfile
+
+# Ensure dependencies are up-to-date
+yarn install --frozen-lockfile --silent
+# Reset all changes in the TF fork to avoid noise from already-applied changes
 cd ../terraform-provider-aws && git checkout . && cd - || exit
-yarn tf-patch apply --cwd ../terraform-provider-aws
+yarn --silent run tf-patch apply --cwd ../terraform-provider-aws
