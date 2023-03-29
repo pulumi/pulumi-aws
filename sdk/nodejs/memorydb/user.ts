011f2d37d5b8157c529199de1b88df1c4c57c924
Thomas Kappler <tkappler@pulumi.com>
2023-03-29 07:12:49 -0700

    Regenerate SDKs with fresh plugins

--- sdk/nodejs/memorydb/user.ts
+++ sdk/nodejs/memorydb/user.ts
@@ -12,24 +12,6 @@ import * as utilities from "../utilities";
  *
  * More information about users and ACL-s can be found in the [MemoryDB User Guide](https://docs.aws.amazon.com/memorydb/latest/devguide/clusters.acls.html).
  *
- * ## Example Usage
- *
- * ```typescript
- * import * as pulumi from "@pulumi/pulumi";
- * import * as aws from "@pulumi/aws";
- * import * as random from "@pulumi/random";
- *
- * const exampleRandomPassword = new random.RandomPassword("exampleRandomPassword", {length: 16});
- * const exampleUser = new aws.memorydb.User("exampleUser", {
- *     userName: "my-user",
- *     accessString: "on ~* &* +@all",
- *     authenticationMode: {
- *         type: "password",
- *         passwords: [exampleRandomPassword.result],
- *     },
- * });
- * ```
- *
  * ## Import
  *
  * Use the `user_name` to import a user. For example
