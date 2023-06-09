bf0b8413515bdec72af3ea884ae40863ee578ff0
Anton Tayanovskyy <anton@pulumi.com>
2023-06-09 12:29:09 -0400

    Add acceptance test for PF based resources

---
+++ examples/examples_yaml_test.go
@@ -0,0 +1,55 @@
+// Copyright 2016-2023, Pulumi Corporation.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//go:build nodejs || all
+// +build nodejs all
+
+package examples
+
+// NOTE about build tags: CI does not understand yaml-specific tags yet, but splits builds per language to run on
+// separate runners. This file piggy-backs on the nodejs tag so yaml tests will run on the same runner as node tests.
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/pulumi/pulumi/pkg/v3/testing/integration"
+)
+
+// This test is not a proper examlpe but a test of the new capability to deploy Plugin Framework and SDKv2 based
+// resources alongside each other in the same provider.
+func TestAccPluginFramework(t *testing.T) {
+	test := getYamlBaseOptions(t).
+		With(integration.ProgramTestOptions{
+			Dir: filepath.Join(getCwd(t), "plugin-framework"),
+		})
+
+	integration.ProgramTest(t, &test)
+}
+
+func getYamlBaseOptions(t *testing.T) integration.ProgramTestOptions {
+	config := map[string]string{}
+	_, usingProfiles := os.LookupEnv("AWS_PROFILE")
+	if !usingProfiles {
+		envRegion := getEnvRegion(t)
+		config = map[string]string{
+			"aws:region": envRegion,
+		}
+	}
+	// Do not use baseOptions here for now as those disable refresh checking. Be extra conservative.
+	return integration.ProgramTestOptions{
+		Config: config,
+	}
+}
