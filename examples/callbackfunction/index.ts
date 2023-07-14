339ddc86c89d8078738da486a4b3d956778238cc
Thomas Kappler <tkappler@pulumi.com>
2023-07-14 12:02:56 +0200

    Use provider object directly instead of wrapping it

--- examples/callbackfunction/index.ts
+++ examples/callbackfunction/index.ts
@@ -16,11 +16,16 @@ import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 
 const config = new pulumi.Config("aws");
-const providerOpts = { provider: new aws.Provider("prov", { region: <aws.Region>config.require("envRegion") }) };
+const provider = new aws.Provider("prov", {
+    region: <aws.Region>config.require("envRegion")
+})
+// const providerOpts = {
+//     provider: provider
+// };
 
 const role = new aws.iam.Role("role", {
     assumeRolePolicy: aws.iam.assumeRolePolicyForPrincipal({ Service: "lambda.amazonaws.com" }),
-}, providerOpts);
+}, { provider });
 
 const policy = new aws.iam.Policy("policy", {
     policy: {
@@ -36,29 +41,29 @@ const policy = new aws.iam.Policy("policy", {
 const functions = [
     new aws.lambda.CallbackFunction("a", {
         callback: async () => ({ success: true }),
-    }, providerOpts),
+    }, { provider }),
     new aws.lambda.CallbackFunction("b", {
         role: role,
         callback: async () => ({ success: true }),
-    }, providerOpts),
+    }, { provider }),
     new aws.lambda.CallbackFunction("c", {
         role: role.arn,
         callback: async () => ({ success: true }),
-    }, providerOpts),
+    }, { provider }),
     new aws.lambda.CallbackFunction("d", {
         policies: [aws.iam.ManagedPolicy.AWSLambdaBasicExecutionRole],
         callback: async () => ({ success: true }),
-    }, providerOpts),
+    }, { provider }),
     new aws.lambda.CallbackFunction("e", {
         callbackFactory: () => {
             const ret = { success: true };
             return async () => ret;
         },
-    }, providerOpts),
-    // new aws.lambda.CallbackFunction("f", {
-    //     policies: { one: policy.arn },
-    //     callback: async () => ({ success: true }),
-    // }, providerOpts),
+    }, { provider }),
+    new aws.lambda.CallbackFunction("f", {
+        policies: { one: policy.arn },
+        callback: async () => ({ success: true }),
+    }, { provider }),
 ];
 
 export const arns = functions.map(f => f.arn);
