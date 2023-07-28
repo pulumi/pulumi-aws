8d286aed414567aa0db550ba93cf4808e9a5e401
Ian Wahbe <ian@wahbe.com>
2023-07-28 15:30:55 +0200

    Test that we set "tags" on creation
    TF does, and if we assume that "tags" is set correctly, we then get the expected diff.

--- provider/tests/provider_test.go
+++ provider/tests/provider_test.go
@@ -41,12 +41,239 @@ func replaySequence(t *testing.T, sequence string) {
 	testutils.ReplaySequence(t, p, sequence)
 }
 
+// We test that when `defaultTags` is populated for the provider, both `tags` and
+// `tagsAll` are populated on the created resource.
+//
+// Because the test actually creates bucket, we run the corresponding "Delete" on the
+// bucket to cleanup.
+func TestTags(t *testing.T) {
+	t.Cleanup(func() {
+		if t.Failed() {
+			t.Logf("Test already failed, cleaning up bucket")
+		}
+		replaySequence(t, `[
+  {
+    "method": "/pulumirpc.ResourceProvider/Configure",
+    "request": {
+      "variables": {
+        "aws:config:defaultTags": "{\"tags\":{\"foo\":\"buzz\"}}",
+        "aws:config:region": "us-west-2",
+        "aws:config:skipCredentialsValidation": "false",
+        "aws:config:skipMetadataApiCheck": "true",
+        "aws:config:skipRegionValidation": "true"
+      },
+      "args": {
+        "defaultTags": "{\"tags\":{\"foo\":\"buzz\"}}",
+        "region": "us-west-2",
+        "skipCredentialsValidation": "false",
+        "skipMetadataApiCheck": "true",
+        "skipRegionValidation": "true"
+      },
+      "acceptSecrets": true,
+      "acceptResources": true,
+      "sendsOldInputs": true
+    },
+    "response": {
+      "supportsPreview": true
+    },
+    "metadata": {
+      "kind": "resource",
+      "mode": "client",
+      "name": "aws"
+    }
+  },
+  {
+    "method": "/pulumirpc.ResourceProvider/Delete",
+    "request": {
+      "id": "b-7242271",
+      "urn": "urn:pulumi:test::secret-random-yaml::aws:s3/bucketV2:BucketV2::b",
+      "properties": {
+        "__meta": "{\"e2bfb730-ecaa-11e6-8f88-34363bc7c4c0\":{\"create\":1200000000000,\"delete\":3600000000000,\"read\":1200000000000,\"update\":1200000000000}}",
+        "accelerationStatus": "",
+        "arn": "arn:aws:s3:::b-7242271",
+        "bucket": "b-7242271",
+        "bucketDomainName": "b-7242271.s3.amazonaws.com",
+        "bucketPrefix": "",
+        "bucketRegionalDomainName": "b-7242271.s3.us-west-2.amazonaws.com",
+        "corsRules": [],
+        "forceDestroy": false,
+        "grants": [
+          {
+            "id": "e07865a5679c7977370948f1f1e51c21b12d8cfdd396a7e3172275d9164e01b8",
+            "permissions": [
+              "FULL_CONTROL"
+            ],
+            "type": "CanonicalUser",
+            "uri": ""
+          }
+        ],
+        "hostedZoneId": "Z3BJ6K6RIION7M",
+        "id": "b-7242271",
+        "lifecycleRules": [],
+        "loggings": [],
+        "objectLockConfiguration": null,
+        "objectLockEnabled": false,
+        "policy": "",
+        "region": "us-west-2",
+        "replicationConfigurations": [],
+        "requestPayer": "BucketOwner",
+        "serverSideEncryptionConfigurations": [
+          {
+            "rules": [
+              {
+                "applyServerSideEncryptionByDefaults": [
+                  {
+                    "kmsMasterKeyId": "",
+                    "sseAlgorithm": "AES256"
+                  }
+                ],
+                "bucketKeyEnabled": false
+              }
+            ]
+          }
+        ],
+        "tags": {},
+        "tagsAll": {
+          "foo": "buz"
+        },
+        "versionings": [
+          {
+            "enabled": false,
+            "mfaDelete": false
+          }
+        ],
+        "websites": []
+      }
+    },
+    "response": {},
+    "metadata": {
+      "kind": "resource",
+      "mode": "client",
+      "name": "aws"
+    }
+  }
+]`)
+	})
+	replaySequence(t, `[
+  {
+    "method": "/pulumirpc.ResourceProvider/Configure",
+    "request": {
+      "variables": {
+        "aws:config:defaultTags": "{\"tags\":{\"foo\":\"buzz\"}}",
+        "aws:config:region": "us-west-2",
+        "aws:config:skipCredentialsValidation": "false",
+        "aws:config:skipMetadataApiCheck": "true",
+        "aws:config:skipRegionValidation": "true"
+      },
+      "args": {
+        "defaultTags": "{\"tags\":{\"foo\":\"buzz\"}}",
+        "region": "us-west-2",
+        "skipCredentialsValidation": "false",
+        "skipMetadataApiCheck": "true",
+        "skipRegionValidation": "true"
+      },
+      "acceptSecrets": true,
+      "acceptResources": true,
+      "sendsOldInputs": true
+    },
+    "response": {
+      "supportsPreview": true
+    },
+    "metadata": {
+      "kind": "resource",
+      "mode": "client",
+      "name": "aws"
+    }
+  },
+  {
+    "method": "/pulumirpc.ResourceProvider/Create",
+    "request": {
+      "urn": "urn:pulumi:test::secret-random-yaml::aws:s3/bucketV2:BucketV2::b",
+      "properties": {
+        "__defaults": [
+          "bucket",
+          "forceDestroy"
+        ],
+        "bucket": "b-7242271",
+        "forceDestroy": false
+      }
+    },
+    "response": {
+      "id": "b-7242271",
+      "properties": {
+        "__meta": "{\"e2bfb730-ecaa-11e6-8f88-34363bc7c4c0\":{\"create\":1200000000000,\"delete\":3600000000000,\"read\":1200000000000,\"update\":1200000000000}}",
+        "accelerationStatus": "",
+        "arn": "arn:aws:s3:::b-7242271",
+        "bucket": "b-7242271",
+        "bucketDomainName": "b-7242271.s3.amazonaws.com",
+        "bucketPrefix": "",
+        "bucketRegionalDomainName": "b-7242271.s3.us-west-2.amazonaws.com",
+        "corsRules": [],
+        "forceDestroy": false,
+        "grants": [
+          {
+            "id": "e07865a5679c7977370948f1f1e51c21b12d8cfdd396a7e3172275d9164e01b8",
+            "permissions": [
+              "FULL_CONTROL"
+            ],
+            "type": "CanonicalUser",
+            "uri": ""
+          }
+        ],
+        "hostedZoneId": "Z3BJ6K6RIION7M",
+        "id": "b-7242271",
+        "lifecycleRules": [],
+        "loggings": [],
+        "objectLockConfiguration": null,
+        "objectLockEnabled": false,
+        "policy": "",
+        "region": "us-west-2",
+        "replicationConfigurations": [],
+        "requestPayer": "BucketOwner",
+        "serverSideEncryptionConfigurations": [
+          {
+            "rules": [
+              {
+                "applyServerSideEncryptionByDefaults": [
+                  {
+                    "kmsMasterKeyId": "",
+                    "sseAlgorithm": "AES256"
+                  }
+                ],
+                "bucketKeyEnabled": false
+              }
+            ]
+          }
+        ],
+        "tags": {
+          "foo": "buzz"
+        },
+        "tagsAll": {
+          "foo": "buzz"
+        },
+        "versionings": [
+          {
+            "enabled": false,
+            "mfaDelete": false
+          }
+        ],
+        "websites": []
+      }
+    },
+    "metadata": {
+      "kind": "resource",
+      "mode": "client",
+      "name": "aws"
+    }
+  }
+]`)
+}
+
 // Repro https://github.com/pulumi/pulumi-aws/issues/2633
 func TestRegression2633(t *testing.T) {
-	t.Run("changed tags",
-		func(t *testing.T) { replaySequence(t, regress2633(true)) })
-	t.Run("removed tags",
-		func(t *testing.T) { replaySequence(t, regress2633(false)) })
+	t.Parallel()
+	t.Run("changed tags", func(t *testing.T) { replaySequence(t, regress2633(true)) })
+	t.Run("removed tags", func(t *testing.T) { replaySequence(t, regress2633(false)) })
 }
 
 func regress2633(hasTags bool) string {
@@ -116,7 +343,7 @@ func regress2633(hasTags bool) string {
             "bucketKeyEnabled": false
           }
         },
-        "tags": {},
+        "tags": { "foo": "bar" },
         "tagsAll": {
           "foo": "bar"
         },
@@ -150,7 +377,9 @@ func regress2633(hasTags bool) string {
     "response": {
       "stables": "*",
       "changes": "DIFF_SOME",
-      "hasDetailedDiff": true
+      "hasDetailedDiff": true,
+      "detailedDiff": { "tags.foo": { "kind": "DELETE" } },
+      "diffs": ["tags"]
     },
     "metadata": {
       "kind": "resource",
