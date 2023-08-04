1fd15a3f2b023aff12065711c504b9878e591b57
Ian Wahbe <ian@wahbe.com>
2023-08-04 12:45:19 +0200

    Remove outdated tests
    I have left skeleton code for GRPC based testing, as I often find it useful when debugging
    locally. I have noted that it does not run in CI, and is just there for local development.

--- provider/tests/go.mod
+++ provider/tests/go.mod
@@ -12,7 +12,7 @@ replace (
 
 require (
 	github.com/pulumi/pulumi-aws/provider/v6 v6.0.0-00010101000000-000000000000
-	github.com/pulumi/pulumi-terraform-bridge/pf v0.14.2-0.20230801105739-e05114319908
+	github.com/pulumi/pulumi-terraform-bridge/pf v0.14.2-0.20230801203955-5d215c892096
 	github.com/pulumi/pulumi-terraform-bridge/testing v0.0.1
 	github.com/stretchr/testify v1.8.4
 )
@@ -167,8 +167,8 @@ require (
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pkg/term v1.1.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/pulumi/pulumi-terraform-bridge/v3 v3.55.1-0.20230801105739-e05114319908 // indirect
-	github.com/pulumi/pulumi-terraform-bridge/x/muxer v0.0.7-0.20230801105739-e05114319908 // indirect
+	github.com/pulumi/pulumi-terraform-bridge/v3 v3.55.1-0.20230801203955-5d215c892096 // indirect
+	github.com/pulumi/pulumi-terraform-bridge/x/muxer v0.0.7-0.20230801203955-5d215c892096 // indirect
 	github.com/pulumi/pulumi/pkg/v3 v3.76.1 // indirect
 	github.com/pulumi/pulumi/sdk/v3 v3.76.1 // indirect
 	github.com/pulumi/terraform-diff-reader v0.0.2 // indirect
--- provider/tests/provider_test.go
+++ provider/tests/provider_test.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package provider
+package test
 
 import (
 	"context"
@@ -34,243 +34,20 @@ func replaySequence(t *testing.T, sequence string) {
 	info := *aws.Provider()
 	ctx := context.Background()
 	p, err := pfbridge.MakeMuxedServer(ctx, info.Name, info,
-		[]byte("{}"), /* We leave the schema blank. This will result in incorrect calls to
-		   GetSchema, but otherwise does not effect the provider. */
+		/*
+		 * We leave the schema blank. This will result in incorrect calls to
+		 * GetSchema, but otherwise does not effect the provider. It reduces the
+		 * time to test start by minutes.
+		 */
+		[]byte("{}"),
 	)(nil)
 	require.NoError(t, err)
 	testutils.ReplaySequence(t, p, sequence)
 }
 
-// We test that when `defaultTags` is populated for the provider, both `tags` and
-// `tagsAll` are populated on the created resource.
+// This serves as a template for GRPC tests on pulumi-aws.
 //
-// Because the test actually creates bucket, we run the corresponding "Delete" on the
-// bucket to cleanup.
-func TestTags(t *testing.T) {
-	replaySequence(t, `[
-  {
-    "method": "/pulumirpc.ResourceProvider/Configure",
-    "request": {
-      "variables": {
-        "aws:config:defaultTags": "{\"tags\":{\"abc\":\"def\",\"foo\":\"buzz\"}}",
-        "aws:config:region": "us-west-2",
-        "aws:config:skipCredentialsValidation": "false",
-        "aws:config:skipMetadataApiCheck": "true",
-        "aws:config:skipRegionValidation": "true"
-      },
-      "args": {
-        "defaultTags": "{\"tags\":{\"abc\":\"def\",\"foo\":\"buzz\"}}",
-        "region": "us-west-2",
-        "skipCredentialsValidation": "true",
-        "skipMetadataApiCheck": "true",
-        "skipRegionValidation": "true"
-      },
-      "acceptSecrets": true,
-      "acceptResources": true,
-      "sendsOldInputs": true
-    },
-    "response": {
-      "supportsPreview": true
-    },
-    "metadata": {
-      "kind": "resource",
-      "mode": "client",
-      "name": "aws"
-    }
-  },
-  {
-    "method": "/pulumirpc.ResourceProvider/Check",
-    "request": {
-      "urn": "urn:pulumi:test::secret-random-yaml::aws:s3/bucketV2:BucketV2::b",
-      "olds": {},
-      "news": {
-        "tags": {
-          "foo": "fizz"
-        }
-      },
-      "randomSeed": "6ba2bL+cZxmiU0dbBQuYcRtIJBjlR6W2hOyKGiafB3Y="
-    },
-    "response": {
-      "inputs": {
-        "__defaults": [
-          "bucket",
-          "forceDestroy"
-        ],
-        "bucket": "b-5b730b2",
-        "forceDestroy": false,
-        "tags": {
-          "__defaults": [],
-          "abc": "def",
-          "foo": "fizz"
-        }
-      }
-    },
-    "metadata": {
-      "kind": "resource",
-      "mode": "client",
-      "name": "aws"
-    }
-  },
-  {
-    "method": "/pulumirpc.ResourceProvider/Create",
-    "request": {
-      "urn": "urn:pulumi:test::secret-random-yaml::aws:s3/bucketV2:BucketV2::b",
-      "properties": {
-        "__defaults": [
-          "bucket",
-          "forceDestroy"
-        ],
-        "bucket": "b-5b730b2",
-        "forceDestroy": false,
-        "tags": {
-          "__defaults": [],
-          "abc": "def",
-          "foo": "fizz"
-        }
-      },
-      "preview": true
-    },
-    "response": {
-      "properties": {
-        "bucket": "b-5b730b2",
-        "forceDestroy": false,
-        "id": "",
-        "tags": {
-          "abc": "def",
-          "foo": "fizz"
-        },
-        "tagsAll": {
-          "abc": "def",
-          "foo": "fizz"
-        }
-      }
-    },
-    "metadata": {
-      "kind": "resource",
-      "mode": "client",
-      "name": "aws"
-    }
-  }
-]`)
-}
-
-// Repro https://github.com/pulumi/pulumi-aws/issues/2633
-func TestRegression2633(t *testing.T) {
-	t.Parallel()
-	t.Run("changed tags", func(t *testing.T) { replaySequence(t, regress2633(true)) })
-	t.Run("removed tags", func(t *testing.T) { replaySequence(t, regress2633(false)) })
-}
-
-func regress2633(hasTags bool) string {
-	var args, vars string
-	if hasTags {
-		args = "\n" + `"defaultTags": "{\"tags\":{\"foo\":\"buzz\"}}",`
-		vars = "\n" + `"aws:config:defaultTags": "{\"tags\":{\"foo\":\"buzz\"}}",`
-	}
-	return `[
-  {
-    "method": "/pulumirpc.ResourceProvider/Configure",
-    "request": {
-      "variables": {` + vars + `
-        "aws:config:region": "us-west-2",
-        "aws:config:skipCredentialsValidation": "true",
-        "aws:config:skipMetadataApiCheck": "true",
-        "aws:config:skipRegionValidation": "true"
-      },
-      "args": {` + args + `
-        "region": "us-west-2",
-        "skipCredentialsValidation": "false",
-        "skipMetadataApiCheck": "true",
-        "skipRegionValidation": "true"
-      },
-      "acceptSecrets": true,
-      "acceptResources": true,
-      "sendsOldInputs": true
-    },
-    "response": {
-      "supportsPreview": true
-    },
-    "metadata": {
-      "kind": "resource",
-      "mode": "client",
-      "name": "aws"
-    }
-  },
-  {
-    "method": "/pulumirpc.ResourceProvider/Diff",
-    "request": {
-      "id": "b-350b1e9",
-      "urn": "urn:pulumi:test::secret-random-yaml::aws:s3/bucket:Bucket::b",
-      "olds": {
-        "accelerationStatus": "",
-        "acl": "private",
-        "arn": "arn:aws:s3:::b-350b1e9",
-        "bucket": "b-350b1e9",
-        "bucketDomainName": "b-350b1e9.s3.amazonaws.com",
-        "bucketRegionalDomainName": "b-350b1e9.s3.us-west-2.amazonaws.com",
-        "corsRules": [],
-        "forceDestroy": false,
-        "grants": [],
-        "hostedZoneId": "Z3BJ6K6RIION7M",
-        "id": "b-350b1e9",
-        "lifecycleRules": [],
-        "loggings": [],
-        "objectLockConfiguration": null,
-        "region": "us-west-2",
-        "replicationConfiguration": null,
-        "requestPayer": "BucketOwner",
-        "serverSideEncryptionConfiguration": {
-          "rule": {
-            "applyServerSideEncryptionByDefault": {
-              "kmsMasterKeyId": "",
-              "sseAlgorithm": "AES256"
-            },
-            "bucketKeyEnabled": false
-          }
-        },
-        "tags": { "foo": "bar" },
-        "tagsAll": {
-          "foo": "bar"
-        },
-        "versioning": {
-          "enabled": false,
-          "mfaDelete": false
-        },
-        "website": null
-      },
-      "news": {
-        "__defaults": [
-          "acl",
-          "bucket",
-          "forceDestroy"
-        ],
-        "acl": "private",
-        "bucket": "b-350b1e9",
-        "forceDestroy": false
-      },
-      "oldInputs": {
-        "__defaults": [
-          "acl",
-          "bucket",
-          "forceDestroy"
-        ],
-        "acl": "private",
-        "bucket": "b-350b1e9",
-        "forceDestroy": false
-      }
-    },
-    "response": {
-      "stables": "*",
-      "changes": "DIFF_SOME",
-      "hasDetailedDiff": true,
-      "detailedDiff": { "tags.foo": { "kind": "DELETE" } },
-      "diffs": ["tags"]
-    },
-    "metadata": {
-      "kind": "resource",
-      "mode": "client",
-      "name": "aws"
-    }
-  }
-]`
+// This test is *not* run in CI, and is purely to serve as a local test for debugging.
+func TestReplay(t *testing.T) {
+	replaySequence(t, `[]`)
 }
