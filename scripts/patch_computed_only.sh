5c5f8b99555ae98caa6b9827c5815b974e525ecc
Anton Tayanovskyy <anton@pulumi.com>
2023-11-03 15:45:14 -0400

    Make tags_all attribute no longer computed for Plugin Framework

---
+++ scripts/patch_computed_only.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# Edit upstream Plugin Framework resources to ensure names.AttrTagsAll maps to tags attributes that
+# are no longer computed, because Pulumi needs them to not be computed.
+
+set -euo pipefail
+
+FILES=$(cd upstream && git grep TagsAttributeComputedOnly |
+        grep -v data_source |
+        grep -v tools/ |
+        grep -v tags/framework/ |
+        grep -v docs/ |
+        grep -v skaff/ |
+        grep -v framework.go |
+        awk -F: '{print $1}')
+
+
+for f in $FILES; do
+    replace="s/names.AttrTagsAll: tftags.TagsAttributeComputedOnly/names.AttrTagsAll: tftags.TagsAttribute/g"
+    perl -i -p -e "$replace" "upstream/$f"
+done
