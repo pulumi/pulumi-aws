056cafe285e4353defd39cb37b788c84856eb53f
Mikhail Shilkov <github@mikhail.io>
2023-11-06 14:14:56 +0100

    Revert Java version change

--- sdk/java/src/main/java/com/pulumi/aws/Utilities.java
+++ sdk/java/src/main/java/com/pulumi/aws/Utilities.java
@@ -57,7 +57,6 @@ public class Utilities {
         return Optional.empty();
 	}
 
-	// TODO: this probably should be done via a mutator on the InvokeOptions
 	public static InvokeOptions withVersion(@Nullable InvokeOptions options) {
             if (options != null && options.getVersion().isPresent()) {
                 return options;
