a9eef085f65dd98b50fafa206967d8eaddae89c0
Ian Wahbe <ian@wahbe.com>
2023-11-27 10:45:35 -0800

    Undepricate CloudWatchFullAccess (#3050)
    6f10e7372075e73139a4e4b6dff69a76db34cd88: From AWS:
    
    > We recently notified you that the AWS Managed Policy
    "CloudWatchFullAccess" would be
    > deprecated on December 7, 2023, in favor of "CloudWatchFullAccessV2".
    However, based on
    > customer feedback we have decided to continue supporting both policies
    and no immediate
    > action is required.
    
    6371953128b54c1cb07d78b13b3bcc30d0764966: Moves this enum list to it's
    own file.
    ea19900b10914f97de2061c973e2ecec7a008806: Break up extremely long lines.

--- sdk/java/src/main/java/com/pulumi/aws/iam/enums/ManagedPolicy.java
+++ sdk/java/src/main/java/com/pulumi/aws/iam/enums/ManagedPolicy.java
@@ -1088,11 +1088,6 @@ import java.util.StringJoiner;
         CloudWatchEventsInvocationAccess("arn:aws:iam::aws:policy/service-role/CloudWatchEventsInvocationAccess"),
         CloudWatchEventsReadOnlyAccess("arn:aws:iam::aws:policy/CloudWatchEventsReadOnlyAccess"),
         CloudWatchEventsServiceRolePolicy("arn:aws:iam::aws:policy/aws-service-role/CloudWatchEventsServiceRolePolicy"),
-        /**
-         * @deprecated
-         * This policy is deprecated and will no longer be supported by AWS after December 7, 2023. Use CloudWatchFullAccessV2 instead.
-         */
-        @Deprecated /* This policy is deprecated and will no longer be supported by AWS after December 7, 2023. Use CloudWatchFullAccessV2 instead. */
         CloudWatchFullAccess("arn:aws:iam::aws:policy/CloudWatchFullAccess"),
         CloudWatchFullAccessV2("arn:aws:iam::aws:policy/CloudWatchFullAccessV2"),
         CloudWatchInternetMonitorServiceRolePolicy("arn:aws:iam::aws:policy/aws-service-role/CloudWatchInternetMonitorServiceRolePolicy"),
