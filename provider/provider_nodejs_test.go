50298298d8cee6a6a69a0afb574207fa8312e457
VenelinMartinov <venelin.v.martinov@gmail.com>
2023-12-05 23:56:07 +0000

    Fix v6 job queue migration (#3091)
    Fixes https://github.com/pulumi/pulumi-aws/issues/3075
    
    The problem was that the tf migration didn't migrate the ARN property of
    the job queue resource.
    
    Looks like this isn't a problem for tf programs, likely because tf does
    a refresh during update.
    
    This PR adds a patch and an upgrade test for this.
    
    ---------
    
    Co-authored-by: Anton Tayanovskyy <anton@pulumi.com>

--- provider/provider_nodejs_test.go
+++ provider/provider_nodejs_test.go
@@ -6,11 +6,13 @@
 package provider
 
 import (
+	"fmt"
 	"os"
 	"path/filepath"
 	"testing"
 
 	"github.com/pulumi/providertest"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestLambdaLayerNew(t *testing.T) {
@@ -33,11 +35,43 @@ func TestRoute53(t *testing.T) {
 	nodeTest(t, filepath.Join("..", "examples", "route53"))
 }
 
+func TestJobQueue(t *testing.T) {
+	simpleNodeTest(t, filepath.Join("test-programs", "job-queue"),
+		providertest.WithSkippedUpgradeTestMode(providertest.UpgradeTestMode_Quick, "Prefer PreviewOnly"),
+		providertest.WithDiffValidation(func(t *testing.T, d providertest.Diffs) {
+			for _, diff := range d {
+				if diff.URN.Name() != "testQueue" {
+					continue
+				}
+				assert.Emptyf(t, diff.Replaces, "Unexpected replace plan for testQueue")
+				for _, changedProp := range diff.Diffs {
+					// Ignoring benign update from nil to empty-map tags.
+					if changedProp == "tags" {
+						continue
+					}
+					if changedProp == "tagsAll" {
+						continue
+					}
+					assert.Fail(t, fmt.Sprintf("Unexpected update for testQueue: %s", changedProp))
+				}
+			}
+		}))
+}
+
 func nodeTest(t *testing.T, dir string, opts ...providertest.Option) {
 	envRegion := getEnvRegion(t)
 	opts = append(opts,
 		providertest.WithConfig("aws:region", "INVALID_REGION"),
 		providertest.WithConfig("aws:envRegion", envRegion),
+	)
+	test(t, dir, opts...)
+}
+
+// This version of nodeTest does not aws:region INVALID_REGION manipulation.
+func simpleNodeTest(t *testing.T, dir string, opts ...providertest.Option) {
+	envRegion := getEnvRegion(t)
+	opts = append(opts,
+		providertest.WithConfig("aws:region", envRegion),
 	)
 	test(t, dir, opts...)
 }
