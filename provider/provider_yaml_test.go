c103b36ece74ca3f6f39b3e1e9227dff3fdf19ee
Venelin <venelin@pulumi.com>
2024-01-08 14:11:02 -0800

    workaround

--- provider/provider_yaml_test.go
+++ provider/provider_yaml_test.go
@@ -6,10 +6,15 @@
 package provider
 
 import (
+	"math/rand"
+	"os"
 	"path/filepath"
 	"testing"
 
 	"github.com/pulumi/providertest"
+	"github.com/pulumi/providertest/pulumitest"
+	"github.com/pulumi/providertest/pulumitest/opttest"
+	"github.com/stretchr/testify/require"
 )
 
 func TestBucket(t *testing.T) {
@@ -83,4 +88,32 @@ func TestKmsKey(t *testing.T) {
 
 func TestSecretsManagerSecret(t *testing.T) {
 	test(t, filepath.Join("test-programs", "secretsmanager-secret"))
+}
+
+var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+func randSeq(n int) string {
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(b)
+}
+
+func TestNonIdempotentSnsTopic(t *testing.T) {
+	cwd, err := os.Getwd()
+	if err != nil {
+		t.Error(err)
+	}
+	ptest := pulumitest.NewPulumiTest(t, filepath.Join("test-programs", "non-idempotent-sns-topic"),
+		opttest.LocalProviderPath("aws", filepath.Join(cwd, "..", "bin")),
+	)
+
+	ptest.InstallStack("test")
+	// generate random name
+	topic_name := randSeq(12)
+	ptest.SetConfig("snsTopicName", topic_name)
+
+	_, err = ptest.CurrentStack().Up(ptest.Context())
+	require.ErrorContains(t, err, "already exists")
 }
