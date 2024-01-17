60501930a889be7d0ab7da0b7f3b1ff2ff29e92e
VenelinMartinov <venelin.v.martinov@gmail.com>
2024-01-17 20:43:18 +0200

    Revert "Non-idempotent SNS topic creation workaround (#3235)" (#3272)
    This reverts commit 976def3faca6418437c095b3d0c5d81d24f99c69.
    
    Until we sort out
    https://github.com/pulumi/pulumi-aws/pull/3268#discussion_r1456138639
    
    It hasn't been released so no need to patch it.

--- provider/provider_yaml_test.go
+++ provider/provider_yaml_test.go
@@ -6,15 +6,10 @@
 package provider
 
 import (
-	"math/rand"
-	"os"
 	"path/filepath"
 	"testing"
 
 	"github.com/pulumi/providertest"
-	"github.com/pulumi/providertest/pulumitest"
-	"github.com/pulumi/providertest/pulumitest/opttest"
-	"github.com/stretchr/testify/require"
 )
 
 func TestBucket(t *testing.T) {
@@ -88,42 +83,4 @@ func TestKmsKey(t *testing.T) {
 
 func TestSecretsManagerSecret(t *testing.T) {
 	test(t, filepath.Join("test-programs", "secretsmanager-secret"))
-}
-
-var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
-
-func randSeq(n int) string {
-	b := make([]rune, n)
-	for i := range b {
-		b[i] = letters[rand.Intn(len(letters))]
-	}
-	return string(b)
-}
-
-func pulumiTest(t *testing.T, dir string) *pulumitest.PulumiTest {
-	if testing.Short() {
-		t.Skipf("Skipping in testing.Short() mode, assuming this is a CI run without AWS creds")
-		return nil
-	}
-	cwd, err := os.Getwd()
-	if err != nil {
-		t.Error(err)
-	}
-	ptest := pulumitest.NewPulumiTest(t, dir,
-		opttest.LocalProviderPath("aws", filepath.Join(cwd, "..", "bin")),
-	)
-
-	return ptest
-}
-
-func TestNonIdempotentSnsTopic(t *testing.T) {
-	ptest := pulumiTest(t, filepath.Join("test-programs", "non-idempotent-sns-topic"))
-
-	ptest.InstallStack("test")
-	// generate random name
-	topic_name := randSeq(12)
-	ptest.SetConfig("snsTopicName", topic_name)
-
-	_, err := ptest.CurrentStack().Up(ptest.Context())
-	require.ErrorContains(t, err, "already exists")
 }
