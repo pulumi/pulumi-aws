59f207aeda0443ed3764a00c6ce9fb073dd94b17
Florian Stadler <florian@pulumi.com>
2024-06-03 23:18:10 +0200

    Fix static get function for ssm.parameter with versioned or labled IDs
    When the upstream SSM resources where moved to the AWS SDK v2 the lookup of
    SSM parameters with versions or labels got broken. This fixes that
    again.

--- examples/examples_nodejs_test.go
+++ examples/examples_nodejs_test.go
@@ -658,3 +658,26 @@ func TestRdsGetEngineVersion(t *testing.T) {
 	engineVersion := res.Outputs["vs"]
 	require.NotEmpty(t, engineVersion.Value)
 }
+
+// Checks static get function for ssm.parameter that was broken for versioned IDs.
+//
+// See https://github.com/pulumi/pulumi-aws/issues/4011
+func TestRegress4011(t *testing.T) {
+	test := getJSBaseOptions(t).
+		With(integration.ProgramTestOptions{
+			Dir: "regress-4011",
+
+
+			EditDirs: []integration.EditDir{{
+				Dir:      filepath.Join("regress-4011"),
+				Additive: true,
+			}},
+		})
+
+	// 
+	// test.SkipEmptyPreviewUpdate = true
+
+	// Disable envRegion mangling
+	test.Config = nil
+	integration.ProgramTest(t, &test)
+}
