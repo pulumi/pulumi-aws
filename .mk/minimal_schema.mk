9b350971635a0215f41be1152210bb740cb6d781
Anton Tayanovskyy <anton@pulumi.com>
2024-10-04 16:37:20 -0400

    Add more Make dependencies

--- .mk/minimal_schema.mk
+++ .mk/minimal_schema.mk
@@ -3,3 +3,11 @@ minimal_schema:		tfgen minimal_schema_no_deps
 minimal_schema_no_deps:
 	echo "Computing minimal schema"
 	cd provider/cmd/pulumi-resource-aws && PULUMI_AWS_MINIMAL_SCHEMA=true VERSION=$(VERSION_GENERIC) go generate
+
+# To work around CI limitations, ensure that minimal schema is rebuilt for embedding right before the provider binary is
+# built for a given platform.
+provider_dist-linux-amd64: minimal_schema_no_deps
+provider_dist-linux-arm64: minimal_schema_no_deps
+provider_dist-darwin-amd64: minimal_schema_no_deps
+provider_dist-darwin-arm64: minimal_schema_no_deps
+provider_dist-windows-amd64: minimal_schema_no_deps
