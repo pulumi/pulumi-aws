ac45be308a506f6320e3dc492c7959703542732c
Anton Tayanovskyy <anton@pulumi.com>
2024-10-25 15:14:46 -0400

    make build_sdks

--- sdk/python/pulumi_aws/iam/role.py
+++ sdk/python/pulumi_aws/iam/role.py
@@ -56,6 +56,9 @@ class RoleArgs:
         if force_detach_policies is not None:
             pulumi.set(__self__, "force_detach_policies", force_detach_policies)
         if inline_policies is not None:
+            warnings.warn("""The inline_policy argument is deprecated. Use the iam.RolePolicy resource instead. If Terraform should exclusively manage all inline policy associations (the current behavior of this argument), use the iam.RolePoliciesExclusive resource as well.""", DeprecationWarning)
+            pulumi.log.warn("""inline_policies is deprecated: The inline_policy argument is deprecated. Use the iam.RolePolicy resource instead. If Terraform should exclusively manage all inline policy associations (the current behavior of this argument), use the iam.RolePoliciesExclusive resource as well.""")
+        if inline_policies is not None:
             pulumi.set(__self__, "inline_policies", inline_policies)
         if managed_policy_arns is not None:
             pulumi.set(__self__, "managed_policy_arns", managed_policy_arns)
@@ -114,6 +117,7 @@ class RoleArgs:
 
     @property
     @pulumi.getter(name="inlinePolicies")
+    @_utilities.deprecated("""The inline_policy argument is deprecated. Use the iam.RolePolicy resource instead. If Terraform should exclusively manage all inline policy associations (the current behavior of this argument), use the iam.RolePoliciesExclusive resource as well.""")
     def inline_policies(self) -> Optional[pulumi.Input[Sequence[pulumi.Input['RoleInlinePolicyArgs']]]]:
         """
         Configuration block defining an exclusive set of IAM inline policies associated with the IAM role. See below. If no blocks are configured, Pulumi will not manage any inline policies in this resource. Configuring one empty block (i.e., `inline_policy {}`) will cause Pulumi to remove _all_ inline policies added out of band on `apply`.
@@ -260,6 +264,9 @@ class _RoleState:
         if force_detach_policies is not None:
             pulumi.set(__self__, "force_detach_policies", force_detach_policies)
         if inline_policies is not None:
+            warnings.warn("""The inline_policy argument is deprecated. Use the iam.RolePolicy resource instead. If Terraform should exclusively manage all inline policy associations (the current behavior of this argument), use the iam.RolePoliciesExclusive resource as well.""", DeprecationWarning)
+            pulumi.log.warn("""inline_policies is deprecated: The inline_policy argument is deprecated. Use the iam.RolePolicy resource instead. If Terraform should exclusively manage all inline policy associations (the current behavior of this argument), use the iam.RolePoliciesExclusive resource as well.""")
+        if inline_policies is not None:
             pulumi.set(__self__, "inline_policies", inline_policies)
         if managed_policy_arns is not None:
             pulumi.set(__self__, "managed_policy_arns", managed_policy_arns)
@@ -349,6 +356,7 @@ class _RoleState:
 
     @property
     @pulumi.getter(name="inlinePolicies")
+    @_utilities.deprecated("""The inline_policy argument is deprecated. Use the iam.RolePolicy resource instead. If Terraform should exclusively manage all inline policy associations (the current behavior of this argument), use the iam.RolePoliciesExclusive resource as well.""")
     def inline_policies(self) -> Optional[pulumi.Input[Sequence[pulumi.Input['RoleInlinePolicyArgs']]]]:
         """
         Configuration block defining an exclusive set of IAM inline policies associated with the IAM role. See below. If no blocks are configured, Pulumi will not manage any inline policies in this resource. Configuring one empty block (i.e., `inline_policy {}`) will cause Pulumi to remove _all_ inline policies added out of band on `apply`.
@@ -1033,6 +1041,7 @@ class Role(pulumi.CustomResource):
 
     @property
     @pulumi.getter(name="inlinePolicies")
+    @_utilities.deprecated("""The inline_policy argument is deprecated. Use the iam.RolePolicy resource instead. If Terraform should exclusively manage all inline policy associations (the current behavior of this argument), use the iam.RolePoliciesExclusive resource as well.""")
     def inline_policies(self) -> pulumi.Output[Sequence['outputs.RoleInlinePolicy']]:
         """
         Configuration block defining an exclusive set of IAM inline policies associated with the IAM role. See below. If no blocks are configured, Pulumi will not manage any inline policies in this resource. Configuring one empty block (i.e., `inline_policy {}`) will cause Pulumi to remove _all_ inline policies added out of band on `apply`.
