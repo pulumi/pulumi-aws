426c3f8ecff314e1f92a3e7b388b381c6f35e252
Florian Stadler <florian@pulumi.com>
2024-11-16 00:39:42 +0100

    Handle writer errors when creating archives for Lambda test (#4775)
    The flaky Lambda test might be caused by errors when flushing & closing
    the writers for the Lambda archives (e.g. out of space).
    
    This change adds handling for those errors, to help understand why the
    files appear to not be found when executing the pulumi programs.
    
    Relates to https://github.com/pulumi/pulumi-aws/issues/4731

--- provider/provider_nodejs_test.go
+++ provider/provider_nodejs_test.go
@@ -21,6 +21,7 @@ import (
 	"github.com/pulumi/pulumi-aws/provider/v6/pkg/elb"
 	"github.com/pulumi/pulumi/pkg/v3/testing/integration"
 	"github.com/pulumi/pulumi/sdk/v3/go/auto/optpreview"
+	"github.com/pulumi/pulumi/sdk/v3/go/common/util/contract"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
@@ -398,7 +399,12 @@ func createLambdaArchive(size int64) (string, error) {
 
 	// Create a new zip archive
 	zipWriter := zip.NewWriter(tempFile)
-	defer zipWriter.Close()
+	defer func() {
+		err := zipWriter.Close()
+		contract.AssertNoErrorf(err, "Failed closing zip archive")
+		err = tempFile.Close()
+		contract.AssertNoErrorf(err, "Failed closing temporary file")
+	}()
 
 	randomDataReader := io.LimitReader(rand.Reader, size)
 
