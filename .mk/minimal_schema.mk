9aa45cbbd4edaefc052910deddf5a43b40789df4
Daniel Bradley <daniel@pulumi.com>
2024-11-27 14:20:28 +0000

    Mark custom targets as phony
    Avoid make creating empty files when running `make --touch provider`.

--- .mk/minimal_schema.mk
+++ .mk/minimal_schema.mk
@@ -3,6 +3,7 @@ minimal_schema:		tfgen minimal_schema_no_deps
 minimal_schema_no_deps:
 	echo "Computing minimal schema"
 	cd provider/cmd/pulumi-resource-aws && PULUMI_AWS_MINIMAL_SCHEMA=true VERSION=$(VERSION_GENERIC) go generate
+.PHONY: minimal_schema minimal_schema_no_deps
 
 # To work around CI limitations, ensure that minimal schema is rebuilt for embedding right before the provider binary is
 # built for a given platform.
