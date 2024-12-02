338c7414328b4180531a62b86042483267110c74
Anton Tayanovskyy <anton@pulumi.com>
2024-12-02 15:07:49 -0500

    Remove minimal_schema_no_deps

--- .mk/minimal_schema.mk
+++ .mk/minimal_schema.mk
@@ -2,16 +2,16 @@ minimal_schema:		provider/cmd/pulumi-resource-aws/schema-minimal-embed.json
 
 .PHONY: minimal_schema
 
-provider/cmd/pulumi-resource-aws/schema-minimal-embed.json:	.make/schema
+provider/cmd/pulumi-resource-aws/schema-minimal-embed.json:	provider/cmd/pulumi-resource-aws/schema.json
 	echo "Computing minimal schema"
 	cd provider/cmd/pulumi-resource-aws && PULUMI_AWS_MINIMAL_SCHEMA=true VERSION=$(VERSION_GENERIC) go generate
 
 # In build_provider.yml workflow, minimal schema needs to be rebuilt right before the provider binary.
-bin/linux-amd64/$(PROVIDER): minimal_schema_no_deps
-bin/linux-arm64/$(PROVIDER): minimal_schema_no_deps
-bin/darwin-amd64/$(PROVIDER): minimal_schema_no_deps
-bin/darwin-arm64/$(PROVIDER): minimal_schema_no_deps
-bin/windows-amd64/$(PROVIDER).exe: minimal_schema_no_deps
+bin/linux-amd64/$(PROVIDER): minimal_schema
+bin/linux-arm64/$(PROVIDER): minimal_schema
+bin/darwin-amd64/$(PROVIDER): minimal_schema
+bin/darwin-arm64/$(PROVIDER): minimal_schema
+bin/windows-amd64/$(PROVIDER).exe: minimal_schema
 
 # In prerequisites.yml workflow, minimal schema needs to be rebuilt right before the provider binary.
-bin/pulumi-resource-aws: minimal_schema_no_deps
+bin/pulumi-resource-aws: minimal_schema
