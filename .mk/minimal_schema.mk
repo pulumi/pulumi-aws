2b50accfa6e6a45791db076bda552e21d0542e3e
Anton Tayanovskyy <anton@pulumi.com>
2024-12-30 11:41:26 -0500

    fix: Restore minimal schema
    Since v6.62.1 minimal schema was not working. This is now fixed.
    
    A change in standard CI scripts aimed at speeding up makefiles broke the assumptions of pulumi-aws specific Makefile extensions in .mk/minimal_schema.mk - that is CI would now run `make --touch provider schema` which would create an empty provider/cmd/pulumi-resource-aws/schema-minimal-embed.json file instead of recalculating it.
    
    The change in question is: https://github.com/pulumi/ci-mgmt/pull/1157
    
    The fix makes `minimal_schema` a PHONY target that may be a bit less efficient for rebuilding but is no longer affected by `make --touch`.
    
    A regression test is added to catch this problem should it reoccur.
    
    Fixes https://github.com/pulumi/pulumi-aws/issues/5016

--- .mk/minimal_schema.mk
+++ .mk/minimal_schema.mk
@@ -1,10 +1,6 @@
-minimal_schema:		provider/cmd/pulumi-resource-aws/schema-minimal-embed.json
-
 .PHONY: minimal_schema
-
-provider/cmd/pulumi-resource-aws/schema-minimal-embed.json:	provider/cmd/pulumi-resource-aws/schema.json
-	echo "Computing minimal schema"
-	cd provider/cmd/pulumi-resource-aws && PULUMI_AWS_MINIMAL_SCHEMA=true VERSION=$(VERSION_GENERIC) go generate
+minimal_schema:
+	@(cd provider/cmd/pulumi-resource-aws && PULUMI_AWS_MINIMAL_SCHEMA=true VERSION=$(VERSION_GENERIC) go generate)
 
 # In build_provider.yml workflow, minimal schema needs to be rebuilt right before the provider binary.
 bin/linux-amd64/$(PROVIDER): minimal_schema
