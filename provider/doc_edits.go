a2be4c93234532ea89dfb497b7d349ff0f082f3d
guineveresaenger <guinevere@pulumi.com>
2025-02-19 16:28:33 -0800

    Add doc edits rule

--- provider/doc_edits.go
+++ provider/doc_edits.go
@@ -29,6 +29,7 @@ func editRules(defaults []tfbridge.DocsEdit) []tfbridge.DocsEdit {
 		fixUpCloudFrontPublicKey,
 		fixUpEcsServiceName,
 		fixUpBucketReplicationConfig,
+		fixUpVpclatticeResourceconfigurationExample,
 		// This fixes up strings such as:
 		//
 		//	name        = "terraform-kinesis-firehose-os",
@@ -231,6 +232,47 @@ var fixUpBucketReplicationConfig = tfbridge.DocsEdit{
 		return content, nil
 	},
 }
+
+var fixUpVpclatticeResourceconfigurationExample = tfbridge.DocsEdit{
+	Path: "vpclattice_resource_configuration.html.markdown",
+	Edit: func(path string, content []byte) ([]byte, error) {
+		fromBytes := []byte(`port_ranges = ["80"]
+
+  resource_configuration_definition {
+    ip_resource {
+      ip_address = "10.0.0.1"
+    }
+  }`)
+		toBytes := []byte(`port_ranges = ["80"]
+
+  protocol="TCP"
+  resource_configuration_definition {
+    ip_resource {
+      ip_address = "10.0.0.1"
+    }
+  }`)
+		if bytes.Contains(content, fromBytes) {
+			content = bytes.ReplaceAll(
+				content,
+				fromBytes,
+				toBytes)
+		} else {
+			// Hard error to ensure we keep this content up to date
+			return nil, fmt.Errorf("could not find text in upstream %s, "+
+				"please verify replace content in doc_edits.go: %s", path, string(fromBytes),
+			)
+		}
+		return content, nil
+	},
+}
+
+//
+//				 targetedReplace(
+//	"vpclattice_resource_configuration.html.markdown",
+//	`port_ranges = ["80"]`,
+//	`port_ranges = ["80"]\n` +
+//		`protocol="TCP"`,
+//)
 
 func reReplace(from string, to string) tfbridge.DocsEdit {
 	fromR, toB := regexp.MustCompile(from), []byte(to)
