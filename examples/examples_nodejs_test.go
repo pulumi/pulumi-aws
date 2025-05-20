a1f6fb33bcc7afb73113e25330225ca6a261792e
guineveresaenger <guinevere@pulumi.com>
2025-05-20 15:09:49 -0700

    Add example for bucket object v2

--- examples/examples_nodejs_test.go
+++ examples/examples_nodejs_test.go
@@ -1185,9 +1185,10 @@ func TestResourceRefsMigrateCleanlyToStringRefs(t *testing.T) {
 	skipIfShort(t)
 	resourceRefMigrateDir := "migrate-resource-refs"
 	dirs := []string{
-		//filepath.Join(resourceRefMigrateDir, "autoscalinggroup"),
-		//filepath.Join(resourceRefMigrateDir, "elasticbeanstalk"),
+		filepath.Join(resourceRefMigrateDir, "autoscalinggroup"),
+		filepath.Join(resourceRefMigrateDir, "elasticbeanstalk"),
 		filepath.Join(resourceRefMigrateDir, "cloudwatch-with-topic"),
+		filepath.Join(resourceRefMigrateDir, "bucketobject"),
 	}
 	cwd, err := os.Getwd()
 	require.NoError(t, err)
