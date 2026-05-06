8b26f0ba90cc3c105b62cfaffd6d1ad1ba035d37
corymhall <43035978+corymhall@users.noreply.github.com>
2026-05-06 06:11:01 -0400

    Replace provider config validation examples

--- provider/configure_test.go
+++ provider/configure_test.go
@@ -16,6 +16,8 @@ package provider
 
 import (
 	"context"
+	"net/http"
+	"net/http/httptest"
 	"os"
 	"strings"
 	"testing"
@@ -25,6 +27,8 @@ import (
 
 	testutils "github.com/pulumi/providertest/replay"
 	pfbridge "github.com/pulumi/pulumi-terraform-bridge/v3/pkg/pf/tfbridge"
+	pulumirpc "github.com/pulumi/pulumi/sdk/v3/proto/go"
+	"google.golang.org/protobuf/types/known/structpb"
 
 	version "github.com/pulumi/pulumi-aws/provider/v7/pkg/version"
 )
@@ -215,30 +219,114 @@ func TestInvalidCredentialsErrorMessage(t *testing.T) {
 	os.Setenv("AWS_REGION", "us-west-2")
 	os.Setenv("AWS_SKIP_CREDENTIALS_VALIDATION", "false")
 
-	replaySequence(t, `
-	[{
-		"method": "/pulumirpc.ResourceProvider/CheckConfig",
-		"request": {
-			"urn": "urn:pulumi:dev::aws_no_creds::pulumi:providers:aws::default_6_18_2",
-			"olds": {},
-			"news": {
-				"version": "6.18.2"
-			}
-		},
-		"response": {
-			"inputs": "*",
-			"failures": [
-				{
-					"reason": "Invalid credentials configured.\nPlease see https://www.pulumi.com/registry/packages/aws/installation-configuration/ for more information about providing credentials.\nNEW: You can use Pulumi ESC to set up dynamic credentials with AWS OIDC to ensure the correct and valid credentials are used.\nLearn more: https://www.pulumi.com/registry/packages/aws/installation-configuration/#dynamically-generate-credentials-via-pulumi-esc"
-				}
-			]
-		},
-		"metadata": {
-			"kind": "resource",
-			"mode": "client",
-			"name": "aws"
-		}
-	}]`)
+	requestCount := 0
+	sts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		requestCount++
+		w.WriteHeader(http.StatusForbidden)
+		_, _ = w.Write([]byte(`<ErrorResponse xmlns="https://sts.amazonaws.com/doc/2011-06-15/">
+  <Error>
+    <Type>Sender</Type>
+    <Code>InvalidClientTokenId</Code>
+    <Message>The security token included in the request is invalid</Message>
+  </Error>
+  <RequestId>01234567-89ab-cdef-0123-456789abcdef</RequestId>
+</ErrorResponse>`))
+	}))
+	t.Cleanup(sts.Close)
+	os.Setenv("AWS_ENDPOINT_URL", sts.URL)
+
+	provider, err := testProviderServer()
+	require.NoError(t, err)
+	resp, err := provider.CheckConfig(context.Background(), &pulumirpc.CheckRequest{
+		Urn:  "urn:pulumi:dev::aws_no_creds::pulumi:providers:aws::default_6_18_2",
+		Olds: mustStruct(t, map[string]any{}),
+		News: mustStruct(t, map[string]any{
+			"version": "6.18.2",
+		}),
+	})
+	require.Greater(t, requestCount, 0)
+	require.NoError(t, err)
+	require.Len(t, resp.Failures, 1)
+
+	reason := resp.Failures[0].Reason
+	require.Contains(t, reason, "Invalid credentials configured.")
+	assertMessage := "Please see https://www.pulumi.com/registry/packages/aws/installation-configuration/ " +
+		"for more information about providing credentials."
+	require.Equal(t, 1, strings.Count(reason, assertMessage))
+	require.Contains(t, reason, "NEW: You can use Pulumi ESC to set up dynamic credentials with AWS OIDC")
+}
+
+func TestCheckConfigRejectsLegacyAssumeRoleConfigKey(t *testing.T) {
+	skipIfNotShort(t)
+	unsetAWSEnv()
+	os.Setenv("AWS_ACCESS_KEY_ID", "test")
+	os.Setenv("AWS_SECRET_ACCESS_KEY", "test")
+	os.Setenv("AWS_REGION", "us-west-2")
+	os.Setenv("AWS_SKIP_CREDENTIALS_VALIDATION", "false")
+
+	provider, err := testProviderServer()
+	require.NoError(t, err)
+	_, err = provider.CheckConfig(context.Background(), &pulumirpc.CheckRequest{
+		Urn:  "urn:pulumi:dev::aws_assume_role::pulumi:providers:aws::default_7_0_0",
+		Olds: mustStruct(t, map[string]any{}),
+		News: mustStruct(t, map[string]any{
+			"accessKey": "test",
+			"assumeRole": map[string]any{
+				"roleArn": "arn:aws:iam::123456789012:role/example",
+			},
+			"region":    "us-west-2",
+			"secretKey": "test",
+			"version":   "7.0.0",
+		}),
+	})
+	require.EqualError(t, err, "invalid config key 'aws:assumeRole', should be 'aws:assumeRoles'")
+}
+
+func TestCheckConfigValidatesAssumeRoles(t *testing.T) {
+	skipIfNotShort(t)
+	unsetAWSEnv()
+	os.Setenv("AWS_CONFIG_FILE", "non-existent/config.json")
+	os.Setenv("AWS_SHARED_CREDENTIALS_FILE", "non-existent/credentials")
+
+	requestCount := 0
+	sts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		requestCount++
+		w.WriteHeader(http.StatusForbidden)
+		_, _ = w.Write([]byte(`<ErrorResponse xmlns="https://sts.amazonaws.com/doc/2011-06-15/">
+  <Error>
+    <Type>Sender</Type>
+    <Code>AccessDenied</Code>
+    <Message>not authorized to perform: sts:AssumeRole</Message>
+  </Error>
+  <RequestId>01234567-89ab-cdef-0123-456789abcdef</RequestId>
+</ErrorResponse>`))
+	}))
+	t.Cleanup(sts.Close)
+	os.Setenv("AWS_ENDPOINT_URL", sts.URL)
+
+	provider, err := testProviderServer()
+	require.NoError(t, err)
+	resp, err := provider.CheckConfig(context.Background(), &pulumirpc.CheckRequest{
+		Urn:  "urn:pulumi:dev::aws_assume_roles::pulumi:providers:aws::default_7_0_0",
+		Olds: mustStruct(t, map[string]any{}),
+		News: mustStruct(t, map[string]any{
+			"accessKey": "test",
+			"assumeRoles": []any{
+				map[string]any{
+					"roleArn": "arn:aws:iam::123456789012:role/example",
+				},
+			},
+			"endpoints": `[{ "sts": "` + sts.URL + `" }]`,
+			"region":    "us-west-2",
+			"secretKey": "test",
+			"version":   "7.0.0",
+		}),
+	})
+	require.Greater(t, requestCount, 0)
+	require.NoError(t, err)
+	require.Len(t, resp.Failures, 1)
+	require.Contains(t, resp.Failures[0].Reason, "unable to validate AWS credentials.")
+	require.Contains(t, resp.Failures[0].Reason, "Cannot assume IAM Role.")
 }
 
 func TestOtherFailureErrorMessage(t *testing.T) {
@@ -289,6 +377,13 @@ func replaySequence(t *testing.T, sequence string) {
 	)(nil)
 	require.NoError(t, err)
 	testutils.ReplaySequence(t, p, sequence)
+}
+
+func mustStruct(t *testing.T, values map[string]any) *structpb.Struct {
+	t.Helper()
+	st, err := structpb.NewStruct(values)
+	require.NoError(t, err)
+	return st
 }
 
 func init() {
